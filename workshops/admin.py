c4e5202fa452204e90695739fba8782d836409ac
Greg Wilson <gvwilson@third-bit.com>
2014-11-30 21:38:01 -0500

    Setting up administration for the Airport model

--- workshops/admin.py
+++ workshops/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from workshops.models import Airport
 
-# Register your models here.
+admin.site.register(Airport)
--- workshops/models.py
+++ workshops/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Airport(models.Model):
+    '''Represent an airport (used to locate instructors).'''
+
+    iata      = models.CharField(max_length=3)
+    fullname  = models.CharField(max_length=100)
+    country   = models.CharField(max_length=100)
+    latitude  = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.iata
