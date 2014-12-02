da2f9abbaa177c35309e700df551e1f0833dd6d5
Greg Wilson <gvwilson@third-bit.com>
2014-12-01 20:50:03 -0500

    Switching to inherited views

--- workshops/views.py
+++ workshops/views.py
@@ -3,7 +3,7 @@ from workshops.models import Site, Event, Person
 
 def index(request):
     '''Home page.'''
-    context = {}
+    context = {'title' : 'Home Page'}
     return render(request, 'workshops/index.html', context)
 
 #------------------------------------------------------------
@@ -11,13 +11,15 @@ def index(request):
 def all_sites(request):
     '''List all sites.'''
     all_sites = Site.objects.order_by('domain')
-    context = {'all_sites' : all_sites}
+    context = {'title' : 'All Sites',
+               'all_sites' : all_sites}
     return render(request, 'workshops/all_sites.html', context)
 
 def site_details(request, site_domain):
     '''List details of a particular site.'''
     site = Site.objects.get(domain=site_domain)
-    context = {'site' : site}
+    context = {'title' : 'Site {0}'.format(site),
+               'site' : site}
     return render(request, 'workshops/site.html', context)
 
 #------------------------------------------------------------
@@ -25,13 +27,15 @@ def site_details(request, site_domain):
 def all_persons(request):
     '''List all persons.'''
     all_persons = Person.objects.order_by('family', 'personal')
-    context = {'all_persons' : all_persons}
+    context = {'title' : 'All Persons',
+               'all_persons' : all_persons}
     return render(request, 'workshops/all_persons.html', context)
 
 def person_details(request, person_id):
     '''List details of a particular person.'''
     person = Person.objects.get(id=person_id)
-    context = {'person' : person}
+    context = {'title' : 'Person {0}'.format(person),
+               'person' : person}
     return render(request, 'workshops/person.html', context)
 
 #------------------------------------------------------------
@@ -39,12 +43,13 @@ def person_details(request, person_id):
 def all_events(request):
     '''List all events.'''
     all_events = Event.objects.order_by('slug')
-    context = {'all_events' : all_events}
+    context = {'title' : 'All Events',
+               'all_events' : all_events}
     return render(request, 'workshops/all_events.html', context)
 
 def event_details(request, event_slug):
     '''List details of a particular event.'''
     event = Event.objects.get(slug=event_slug)
-    context = {'event' : event}
+    context = {'title' : 'Event {0}'.format(event),
+               'event' : event}
     return render(request, 'workshops/event.html', context)
-
