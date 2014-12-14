4a36f7fa37fef63152bae244c9129b3c9d3b4a81
Rob Beagrie <rob@beagrie.com>
2014-12-14 17:52:30 +0000

    Add the ability to request all items.

--- workshops/views.py
+++ workshops/views.py
@@ -60,26 +60,39 @@ def person_details(request, person_id):
 def all_events(request):
     '''List all events.'''
 
+    EVENTS_PER_PAGE = 25
+
     all_events = Event.objects.order_by('slug')
 
     # Get the number of items requested per page, default to 25
     # This is important for unit testing, we need to be
     # able to specify how many items to expect
-    items = request.GET.get('items_per_page', 25)
+    items = request.GET.get('items_per_page', EVENTS_PER_PAGE)
 
-    events_paginator = Paginator(all_events, items)
+    # Only paginate if the number of items is not 'all'
+    if not items == 'all':
 
-    # Get the page number requested, if any
-    page = request.GET.get('page')
+        # If items is not an integer, set it to the default
+        try:
+            items = int(items)
+        except ValueError:
+            items = EVENTS_PER_PAGE
 
-    try:
-        events = events_paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        events = events_paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        events = events_paginator.page(events_paginator.num_pages)
+        events_paginator = Paginator(all_events, items)
+
+        # Get the page number requested, if any
+        page = request.GET.get('page')
+
+        try:
+            events = events_paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            events = events_paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            events = events_paginator.page(events_paginator.num_pages)
+    else:
+        events = all_events
 
     context = {'title' : 'All Events',
                'all_events' : events}
