8051c7844bf5163aa269e791829dfcbd9ae95339
W. Trevor King <wking@tremily.us>
2014-12-14 19:40:15 -0800

    workshops/migrations/0021_auto_20141214_2232.py: Add data migration
    This name consolidation is almost exactly the example given in the
    Django docs [1].  I haven't separated the schema migration from the
    data migration, because it's a simple enough migration and splitting
    it into three (add Person.name, populate it, and remove the other name
    fields) seemed like overkill.
    
    This migration condenses our earlier personal-middle-family ordering
    to a single point in time.  Users who perfer other oderings will have
    their names mangled by the data migration, but they can fix the
    ordering after the migration and then enjoy the correct ordering ;).
    
    [1]: https://docs.djangoproject.com/en/1.7/topics/migrations/#data-migrations

--- workshops/migrations/0021_auto_20141214_2232.py
+++ workshops/migrations/0021_auto_20141214_2232.py
@@ -4,6 +4,19 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 
 
+def combine_names(apps, schema_editor):
+    # We can't import the Person model directly as it may be a newer
+    # version than this migration expects. We use the historical version.
+    Person = apps.get_model('workshops', 'Person')
+    for person in Person.objects.all():
+        if person.middle:
+            person.name = '{0.personal} {0.middle} {0.family}'.format(person)
+        else:
+            person.name = '{0.personal} {0.family}'.format(person)
+        person.save()
+
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -11,6 +24,13 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.AddField(
+            model_name='person',
+            name='name',
+            field=models.CharField(default='', max_length=100),
+            preserve_default=False,
+        ),
+        migrations.RunPython(combine_names),
         migrations.RemoveField(
             model_name='person',
             name='family',
@@ -22,11 +42,5 @@ class Migration(migrations.Migration):
         migrations.RemoveField(
             model_name='person',
             name='personal',
-        ),
-        migrations.AddField(
-            model_name='person',
-            name='name',
-            field=models.CharField(default='', max_length=100),
-            preserve_default=False,
         ),
     ]
