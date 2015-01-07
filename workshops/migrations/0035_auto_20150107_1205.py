8ee2f2b4c3a0ac40c6b7582a2cf3724f30f41dae
W. Trevor King <wking@tremily.us>
2015-01-07 09:15:08 -0800

    workshops/migrations/0035_auto_20150107_1205.py: Add data migration
    Following the Django docs [1].  I haven't separated the schema
    migration from the data migration, because it's a simple enough
    migration and splitting it into three (add Event.tags, populate it,
    and remove Event.project) seemed like overkill.
    
    [1]: https://docs.djangoproject.com/en/1.7/topics/migrations/#data-migrations

--- workshops/migrations/0035_auto_20150107_1205.py
+++ workshops/migrations/0035_auto_20150107_1205.py
@@ -4,6 +4,15 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 
 
+def copy_project_to_tags(apps, schema_editor):
+    Event = apps.get_model('workshops', 'Event')
+    for event in Event.objects.all().exclude(project=None):
+        tag = event.project
+        print('add {} to {}'.format(tag, event))
+        event.tags.add(tag)
+        event.save()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -15,14 +24,15 @@ class Migration(migrations.Migration):
             old_name='Project',
             new_name='Tag',
         ),
-        migrations.RemoveField(
-            model_name='event',
-            name='project',
-        ),
         migrations.AddField(
             model_name='event',
             name='tags',
             field=models.ManyToManyField(to='workshops.Tag'),
             preserve_default=True,
+        ),
+        migrations.RunPython(copy_project_to_tags),
+        migrations.RemoveField(
+            model_name='event',
+            name='project',
         ),
     ]
