05ddd1b8b3ecca8abb99b395fb21c4f6ea57d7f1
Greg Wilson <gvwilson@third-bit.com>
2015-01-16 08:45:05 -0500

    Asking Django to report unfilled template variables

--- amy/settings.py
+++ amy/settings.py
@@ -82,3 +82,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Warn viewers of invalid template strings                                                                                                            
+TEMPLATE_STRING_IF_INVALID = 'XXX-unset-variable-XXX'
