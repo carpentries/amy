f4a271fdd9d15fe7e15b4cba01476a80da82e9c2
Greg Wilson <gvwilson@third-bit.com>
2015-01-22 09:49:42 -0500

    Merging migrations

---
+++ workshops/migrations/0044_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('workshops', '0040_auto_20150120_1610'),
+        ('workshops', '0043_auto_20150119_2113'),
+    ]
+
+    operations = [
+    ]
