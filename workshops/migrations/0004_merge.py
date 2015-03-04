b420810a8dd2399b522e070a4af46c428f545ffe
Greg Wilson <gvwilson@third-bit.com>
2015-03-04 06:50:08 -0500

    Merging migrations

---
+++ workshops/migrations/0004_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('workshops', '0003_remove_person_slug'),
+        ('workshops', '0003_auto_20150227_1245'),
+    ]
+
+    operations = [
+    ]
