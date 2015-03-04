1ab924299a63a45805dfbab061355d0185b59c04
Greg Wilson <gvwilson@third-bit.com>
2015-03-04 06:47:50 -0500

    Moving auxiliary test script into sub-directory

--- scripts/test-command-line-upload.sh
+++ scripts/test-command-line-upload.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
+# Semi-manual testing of command-line upload.
+
 for i in upload-person-3.csv upload-person-3-dupname.csv upload-person-task-3.csv upload-empty-middle.csv upload-missing-email.csv upload-missing-family.csv upload-missing-personal-email.csv
 do
     echo $i
