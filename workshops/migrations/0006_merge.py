88768a1c2edb8be65a41e3dc187deb8a66c1d734
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2015-05-09 21:25:59 +0200

    Merged two conflicting (by name) migrations
    Two different migrations were submitted via two PRs and because
    of their prefix (0005) they were conflicting.  This is simply
    a merge generated with `./manage.py makemigrations --merge`.

---
+++ workshops/migrations/0006_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('workshops', '0005_auto_20150404_0953'),
+        ('workshops', '0005_auto_20150506_0848'),
+    ]
+
+    operations = [
+    ]
