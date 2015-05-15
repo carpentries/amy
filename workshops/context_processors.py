b2084880ce0061529858c886d48c326643a5c1d0
W. Trevor King <wking@tremily.us>
2015-05-15 10:15:46 -0700

    workshops/context_processors.py: Expose Git version information
    If it exists.

--- workshops/context_processors.py
+++ workshops/context_processors.py
@@ -1,5 +1,17 @@
 from . import __version__
+try:
+    from . import git_version
+except ImportError:
+    git_version = None
 
 
 def version(request):
-    return {'workshops_version': __version__}
+    data = {'workshops_version': __version__}
+    if git_version:
+        data.update({
+            'workshops_git_hash': git_version.HASH,
+            'workshops_git_short_hash': git_version.SHORT_HASH,
+            'workshops_git_date': git_version.DATE,
+            'workshops_git_dirty': git_version.DIRTY,
+        })
+    return data
