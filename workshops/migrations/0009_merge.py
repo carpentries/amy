65030e2f2db36924f22bbb84d8c1f587d87425df
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2015-05-31 20:53:34 +0200

    Merge non-conflicting migrations (0007, 0008)

---
+++ workshops/migrations/0009_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('workshops', '0008_person_notes'),
+        ('workshops', '0008_auto_20150525_1547'),
+    ]
+
+    operations = [
+    ]
