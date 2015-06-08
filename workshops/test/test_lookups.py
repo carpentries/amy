b2cb6cd8461cfb9ec748754d7d314acc12221769
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2015-06-08 09:57:00 +0200

    Add regression test for login-required lookups
    New test makes sure that we never accidentally disable @login_required
    on our lookups.

---
+++ workshops/test/test_lookups.py
@@ -0,0 +1,17 @@
+from django.core.urlresolvers import reverse
+
+from .base import TestBase
+
+
+class TestLookups(TestBase):
+    """Test suite for django-selectable lookups."""
+
+    def test_login_regression(self):
+        """Make sure lookups are login-protected"""
+        url_name = 'selectable-lookup'
+        rv = self.client.get(reverse(url_name, args=['workshops-sitelookup']))
+        assert rv.status_code == 401  # unauthorized
+
+        self._setUpUsersAndLogin()
+        rv = self.client.get(reverse(url_name, args=['workshops-sitelookup']))
+        assert rv.status_code == 200
