c71cd88b98a519431e76440b0f9c48fbe2f49b03
Greg Wilson <gvwilson@third-bit.com>
2015-06-12 09:49:03 -0400

    Updating tests

---
+++ workshops/migrations/0017_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('workshops', '0012_auto_20150612_0807'),
+        ('workshops', '0016_merge'),
+    ]
+
+    operations = [
+    ]
