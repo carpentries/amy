8d9d8ab1391a75e572244be4edad5f05b2f8ce20
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2015-06-12 13:25:27 +0200

    Turn off Django Admin completely
    Plus remove basic admin forms for workshops application.
    We don't need them.

--- amy/urls.py
+++ amy/urls.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^workshops/', include('workshops.urls')),
-    url(r'^admin/', include(admin.site.urls)),
     # url(r'^account/', include('django.contrib.auth.urls')),
 
     # django views for authentication
