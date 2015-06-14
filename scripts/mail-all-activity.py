24307575188e8106a341d8ac86a022294f070772
Greg Wilson <gvwilson@third-bit.com>
2015-06-14 10:59:52 -0400

    Updating mailout script

--- scripts/mail-all-activity.py
+++ scripts/mail-all-activity.py
@@ -4,22 +4,23 @@
 
 import sys
 import yaml
+import smtplib
+from email.mime.text import MIMEText
 
 
 USAGE = 'Usage: mail-all-activity [-r|--real] [filename]'
 
-SUBJECT = 'Checking your Software Carpentry information'
+SUBJECT = 'Updating your Software Carpentry information'
 
-BODY = '''We would like to bring our records up to date, and would be grateful
-if you could check the information below and send us corrections or additions
-by filling in the form at:
+BODY = '''We are changing the way we track the lessons that people are
+comfortable teaching, and would also like to update our list of where
+people are and what they've taught.  We would therefore be grateful if
+you could take ninety seconds to fill in:
 
-    https://docs.google.com/forms/d/1NA2z3mXnrz4ZfEV02rk6vwdK4M180_sYpVjUQBt-yMc/viewform
+  https://docs.google.com/forms/d/1NA2z3mXnrz4ZfEV02rk6vwdK4M180_sYpVjUQBt-yMc/viewform
 
-If the topics you're comfortable teaching aren't listed, or are incorrect,
-please also fill in the form.  And if you would rather not receive mail from
-us in future, please reply to this message and we'll remove you from our
-list.
+If you would rather not receive mail from us in future, please reply
+to this message and we'll remove you from our contact list.
 
 name: {name}
 preferred email: {email}
@@ -33,30 +34,37 @@ def main(argv):
     '''Main driver.'''
 
     # Setup.
-    prog_name, args = argv[0], argv[1:]
     sender = display
+    connection = None
+    prog_name, args = argv[0], argv[1:]
 
     # Default is dummy run - only actually send mail if told to.
     if args and (args[0] in ('-r', '--real')):
+        connection = smtplib.SMTP('mail.third-bit.com', 587)
         sender = send
         args = args[1:]
 
     # If no filename provided, use stdin.
     if len(args) == 0:
-        process(sender, sys.stdin)
+        process(sender, connection, sys.stdin)
     elif len(args) == 1:
         with open(args[0], 'r') as reader:
-            process(sender, reader)
+            process(sender, connection, reader)
     else:
         fail(USAGE)
 
+    # Close SMTP connection.
+    if connection is not None:
+        connection.quit()
 
-def process(sender, reader):
+
+def process(sender, connection, reader):
     '''Process the YAML data loaded through reader.'''
+
     info = yaml.load(reader)
     for record in info:
         address, subject, body = make_message(record)
-        sender(address, subject, body)
+        sender(connection, address, subject, body)
 
 
 def make_message(record):
@@ -67,13 +75,13 @@ def make_message(record):
 
     address = record['email']
 
-    record['optional'] = ''
-    for key in ('airport', 'twitter', 'github'):
-        record['optional'] += format_optional(record, key)
+    record['optional'] = '\n'.join(
+        [format_optional(record, key) for key in ('airport', 'twitter', 'github')])
 
     record['can_teach'] = format_can_teach(record['can_teach'])
 
-    record['tasks'] = format_tasks(record['tasks'])
+    record['tasks'] = '\n'.join(
+        [format_task(t) for t in record['tasks']])
 
     body = BODY.format(**record)
     return address, SUBJECT, body
@@ -83,47 +91,50 @@ def format_optional(record, key):
     '''Format an optional entry for inclusion in the message body.'''
 
     if record.get(key, None):
-        return 'Your {0} is "{1}"\n'.format(key, record[key])
+        return 'Your {0} is "{1}"'.format(key, record[key])
     else:
-        return 'Your {0} is unknown\n'.format(key)
+        return 'Your {0} is unknown'.format(key)
 
 
 def format_can_teach(can_teach):
     '''Format zero or more skills for inclusion in the message body.'''
 
     if can_teach:
-        return 'You can teach: {0}\n'.format(', '.join(can_teach))
-    else:
-        return ''
+        return 'You can teach: {0}'.format(', '.join(can_teach))
+    return ''
 
 
-def format_tasks(tasks):
+def format_task(task):
     '''Format tasks and collaborators for inclusion in the message body.'''
 
-    result = ''
-    for t in tasks:
-        entry = 'You were a {0} at {1} with {2}\n'.format(
-            t['role'],
-            t['slug'],
-            ', '.join(t['others']))
-        result += entry
-    return result
+    return 'You were a {0} at {1} with {2}'.format(
+        task['role'],
+        task['slug'],
+        ', '.join(task['others']))
 
 
-def display(address, subject, body):
+def display(connection, address, subject, body):
     '''Display a message that is not being sent.'''
+
     print('To:', address)
     print('Subject:', subject)
     print(body)
+    print()
 
 
-def send(address, subject, body):
+def send(connection, address, subject, body):
     '''Send email.'''
-    fail('Not yet able to send')
 
+    message = MIMEText(body)
+    message['Subject'] = subject
+    message['From'] = 'admin@software-carpentry.org'
+    message['To'] = address
+    connection.sendmail(message['From'], [message['To']], message.as_string())
+    print(address, file=sys.stderr)
 
 def fail(msg):
     '''Halt and catch fire.'''
+
     print(msg, file=sys.stderr)
     sys.exit(1)
 
