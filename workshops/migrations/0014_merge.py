de968e11085afe995215217687559867a4fc2f64
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2015-06-18 23:44:57 +0200

    Add merge migration

---
+++ workshops/migrations/0014_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('workshops', '0011_auto_20150618_0231'),
+        ('workshops', '0013_auto_20150611_0853'),
+    ]
+
+    operations = [
+    ]
