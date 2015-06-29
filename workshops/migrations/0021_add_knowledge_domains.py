be5d3e4aba4987c46d5fb88a422f37ce93ba8cd4
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2015-06-29 19:17:44 +0200

    Data migration: add knowledge domains

---
+++ workshops/migrations/0021_add_knowledge_domains.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+DOMAINS = [
+    'Space sciences',
+    'Planetary sciences (geology, climatology, oceanography, etc.)',
+    'Physics',
+    'Chemistry',
+    'Organismal biology (ecology, botany, zoology, microbiology)',
+    'Genetics, genomics, bioinformatics',
+    'Medicine',
+    'Civil, mechanical, or chemical engineering',
+    'Computer science/electrical engineering',
+    'Economics/business',
+    'Social sciences',
+    'Psychology',
+    'Humanities',
+    'Library and information science',
+    'Education',
+]
+
+
+def add_knowledge_domains(apps, schema_editor):
+    '''Add instances of KnowledgeDomains.'''
+    KnowledgeDomain = apps.get_model('workshops', 'KnowledgeDomain')
+    for domain in DOMAINS:
+        KnowledgeDomain.objects.create(name=domain)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('workshops', '0020_person_domains'),
+    ]
+
+    operations = [
+        migrations.RunPython(add_knowledge_domains),
+    ]
