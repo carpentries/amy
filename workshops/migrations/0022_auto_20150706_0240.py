f8ca3f3a988effacd2926d673ca78fc359c5e9de
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2015-07-06 09:41:39 +0200

    Add verbose_name ("Eventbrite key") to Event.reg_key
    Fixes #404.

---
+++ workshops/migrations/0022_auto_20150706_0240.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('workshops', '0021_add_knowledge_domains'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='event',
+            name='reg_key',
+            field=models.CharField(blank=True, max_length=20, null=True, verbose_name='Eventbrite key'),
+        ),
+    ]
--- workshops/models.py
+++ workshops/models.py
@@ -294,7 +294,7 @@ class Event(models.Model):
     slug       = models.CharField(max_length=STR_LONG, null=True, blank=True)
     url        = models.CharField(max_length=STR_LONG, unique=True, null=True, blank=True,
                                   help_text='Setting this and startdate "publishes" the event.')
-    reg_key    = models.CharField(max_length=STR_REG_KEY, null=True, blank=True)
+    reg_key    = models.CharField(max_length=STR_REG_KEY, null=True, blank=True, verbose_name="Eventbrite key")
     attendance = models.IntegerField(null=True, blank=True)
     admin_fee  = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     invoiced   = models.NullBooleanField(default=False, blank=True)
