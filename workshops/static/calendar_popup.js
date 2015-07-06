c1f60529fc9c05fcc7bd52380e4d36966729a6c3
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2015-07-06 11:07:55 +0200

    Move JS code for popping up calendar to sep. file
    Fixes #410.
    
    Enhance calendar logic:
    1. on new-event page, the first time user selects start date for an
       event, the end date is set to +1d to that start date
    2. on new-event page, if the end date is selected first, then selecting
       start date won't override selected end date
    
    Also some default settings for calendar widget was added:
    1. a clear button
    2. autoclosing after user selects a date

---
+++ workshops/static/calendar_popup.js
@@ -0,0 +1,28 @@
+var DATEPICKER_DEFAULTS = {
+    autoclose: true,
+    clearBtn: true,
+    format: "yyyy-mm-dd",
+    todayHighlight: true
+};
+
+var first_startdate_selected = false;
+
+$(document).ready(function() {
+    $("#id_start, #id_end").datepicker(DATEPICKER_DEFAULTS);
+    $("#id_event-start, #id_event-end").datepicker(DATEPICKER_DEFAULTS);
+
+    $('#id_start').on('changeDate', function(e) {
+        // if user selects start date for the first time, set end date to +1d
+        if (e.date && !first_startdate_selected) {
+            first_startdate_selected = true;
+            d = e.date;
+            d.setUTCDate(d.getUTCDate() + 1);  // +1d to the start date
+            $('#id_end').datepicker('update', d);
+        }
+    });
+    $('#id_end').on('changeDate', function(e) {
+        // disallow changing end date by logic if the end date was selected
+        // earlier than start date
+        first_startdate_selected = true;
+    });
+});
