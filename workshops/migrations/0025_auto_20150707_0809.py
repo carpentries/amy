ed908c4b68132499b49a66f71e526b4af8333228
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2015-07-07 18:43:57 +0200

    Fix some styling

--- workshops/migrations/0025_auto_20150707_0809.py
+++ workshops/migrations/0025_auto_20150707_0809.py
@@ -6,16 +6,20 @@ from django.db import models, migrations
 
 def change_knowledgedomain_names(apps, schema_editor):
     '''Change "Psychology" → "Psychology/neuroscience".
-    Change "Civil, mechanical, or chemical engineering" → "Civil, mechanical, chemical, or nuclear engineering".
+    Change "Civil, mechanical, or chemical engineering" to
+    "Civil, mechanical, chemical, or nuclear engineering".
     Add "Mathematics/statistics" and "High performance computing".'''
     KnowledgeDomain = apps.get_model('workshops', 'KnowledgeDomain')
-    KnowledgeDomain.objects.filter(name='Psychology').update(name='Psychology/neuroscience')
-    KnowledgeDomain.objects.filter(name='Civil, mechanical, or chemical engineering').update(name='Civil, mechanical, chemical, or nuclear engineering')
+    KnowledgeDomain.objects.filter(name='Psychology') \
+                           .update(name='Psychology/neuroscience')
+    KnowledgeDomain.objects \
+        .filter(name='Civil, mechanical, or chemical engineering') \
+        .update(name='Civil, mechanical, chemical, or nuclear engineering')
     KnowledgeDomain.objects.create(name='Mathematics/statistics')
     KnowledgeDomain.objects.create(name='High performance computing')
 
-class Migration(migrations.Migration):
 
+class Migration(migrations.Migration):
     dependencies = [
         ('workshops', '0024_merge'),
     ]
