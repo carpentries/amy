abb3234adcc016e921e8219d2de25f685d1a2b01
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2015-07-08 23:04:48 +0200

    Fix tests for matching instructors
    3 tests were removed since they tested matching specific number of
    instructors returned ('wanted' instructors).
    
    1 test was added: it tests matching by country.
    
    self.client invocations were fixed (POST → GET).

--- workshops/test/test_instructors.py
+++ workshops/test/test_instructors.py
@@ -9,51 +9,11 @@ class TestLocateInstructors(TestBase):
         super().setUp()
         self._setUpUsersAndLogin()
 
-    def test_search_for_one_instructor_by_airport(self):
-        response = self.client.post(reverse('instructors'),
-                                    {'airport' : self.airport_0_0.id,
-                                     'wanted' : 1})
-        assert response.status_code == 200
-        content = response.content.decode('utf-8')
-        assert "Hermione Granger" in content
-        assert "Harry Potter" not in content
-        assert "Ron Weasley" not in content
-
-    def test_search_for_two_instructors_by_airport(self):
-        response = self.client.post(reverse('instructors'),
-                                    {'airport' : self.airport_0_0.id,
-                                     'wanted' : 2})
-        assert response.status_code == 200
-        content = response.content.decode('utf-8')
-        assert "Hermione Granger" in content
-        assert "Harry Potter" in content
-        assert "Ron Weasley" not in content
-
-    def test_search_for_one_instructor_near_airport(self):
-        response = self.client.post(reverse('instructors'),
-                                    {'airport' : self.airport_55_105.id,
-                                     'wanted' : 1})
-        assert response.status_code == 200
-        content = response.content.decode('utf-8')
-        assert "Hermione Granger" not in content
-        assert "Harry Potter" not in content
-        assert "Ron Weasley" in content
-
-    def test_search_for_one_instructor_near_latlong(self):
-        response = self.client.post(reverse('instructors'),
-                                    {'latitude' : 5,
-                                     'longitude' : 45,
-                                     'wanted' : 1})
-        assert response.status_code == 200
-        content = response.content.decode('utf-8')
-        assert "Hermione Granger" not in content
-        assert "Harry Potter" in content
-        assert "Ron Weasley" not in content
-
     def test_non_instructors_not_returned_by_search(self):
-        response = self.client.post(reverse('instructors'),
-                                    {'airport' : self.airport_0_0.id,
-                                     'wanted' : 1000})
+        response = self.client.get(
+            reverse('instructors'),
+            {'airport_1': self.airport_0_0.pk, 'submit': 'Submit'}
+        )
         assert response.status_code == 200
         content = response.content.decode('utf-8')
         assert "Hermione Granger" in content
@@ -61,10 +21,11 @@ class TestLocateInstructors(TestBase):
         assert "Ron Weasley" in content
 
     def test_match_instructors_on_one_skill(self):
-        response = self.client.post(reverse('instructors'),
-                                    {'airport' : self.airport_50_100.id,
-                                     'swc/git' : 'on',
-                                     'wanted' : 1000})
+        response = self.client.get(
+            reverse('instructors'),
+            {'airport_1': self.airport_50_100.pk, 'lessons': [self.git.pk],
+             'submit': 'Submit'}
+        )
         assert response.status_code == 200
         content = response.content.decode('utf-8')
         assert "Hermione Granger" in content
@@ -72,11 +33,27 @@ class TestLocateInstructors(TestBase):
         assert "Ron Weasley" in content
 
     def test_match_instructors_on_two_skills(self):
-        response = self.client.post(reverse('instructors'),
-                                    {'airport' : self.airport_50_100.id,
-                                     'swc/git' : 'on',
-                                     'dc/sql' : 'on',
-                                     'wanted' : 1000})
+        response = self.client.get(
+            reverse('instructors'),
+            {'airport_1': self.airport_50_100.pk,
+             'lessons': [self.git.pk, self.sql.pk],
+             'submit': 'Submit'}
+        )
+        assert response.status_code == 200
+        content = response.content.decode('utf-8')
+        assert "Hermione Granger" in content
+        assert "Harry Potter" not in content
+        assert "Ron Weasley" not in content
+
+    def test_match_by_country(self):
+        # set up country for an airport, 'cause it doesn't have one
+        self.airport_0_0.country = 'GB'
+        self.airport_0_0.save()
+
+        response = self.client.get(
+            reverse('instructors'),
+            {'country': 'GB', 'submit': 'Submit'}
+        )
         assert response.status_code == 200
         content = response.content.decode('utf-8')
         assert "Hermione Granger" in content
