43c3c1bb12d61a850e701a7fcf502c6bcd5fa6b2
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2015-08-28 00:21:13 +0200

    Migration: add fixtures for academic and computing exp. levels
    These fixtures are most probably constant (at least for a while).
    They correspond (ie. were taken from) with field values on
    http://software-carpentry.org/workshops/request.html

---
+++ workshops/migrations/0040_auto_20150827_1715.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+def add_academic_levels(apps, schema_editor):
+    AcademicLevel = apps.get_model('workshops', 'AcademicLevel')
+    L = [
+        'Undergraduate or below',
+        'Graduate',
+        'Post-doctorate',
+        'Faculty',
+        'Industry',
+        'Don\'t know yet',
+    ]
+    for name in L:
+        AcademicLevel.objects.create(name=name)
+
+
+def add_comp_exp_levels(apps, schema_editor):
+    ComputingExperienceLevel = apps.get_model('workshops',
+                                              'ComputingExperienceLevel')
+    L = [
+        'Novice (uses a spreadsheet for data analysis rather than writing '
+            'code)',
+        'Intermediate (can write a few lines of code for personal use)',
+        'Proficient (writes multi-page programs which may be shared with '
+            'others)',
+        'Don\'t know yet',
+    ]
+    for name in L:
+        ComputingExperienceLevel.objects.create(name=name)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('workshops', '0039_academiclevel_computingexperiencelevel_eventrequest'),
+    ]
+
+    operations = [
+        migrations.RunPython(add_academic_levels),
+        migrations.RunPython(add_comp_exp_levels),
+    ]
