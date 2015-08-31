e351f70bfe3d1cdd1eb352e88bafe50686155aa0
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2015-08-31 18:57:52 +0200

    Add template tag for nav item disabled if perms not found
    The rendered topbar navigation item will be not clickable if the
    specified permissions aren't possesed by user.

--- workshops/templatetags/navigation.py
+++ workshops/templatetags/navigation.py
@@ -4,6 +4,28 @@ from django.core.urlresolvers import reverse
 register = template.Library()
 
 
+def make_element(title, url, active=False, disabled=False):
+    screen_reader = ''
+    classes = []
+
+    if disabled:
+        classes.append('disabled')
+
+    if active:
+        classes.append('active')
+        screen_reader = '<span class="sr-only">(active)</span>'
+
+    if classes:
+        classes = ' '.join(classes)
+        template = ('<li class="{classes}"><a href="{url}">{title} '
+                    '{screen_reader}</a></li>')
+        return template.format(classes=classes, url=url, title=title,
+                               screen_reader=screen_reader)
+    else:
+        template = ('<li><a href="{url}">{title}</a></li>')
+        return template.format(url=url, title=title)
+
+
 @register.simple_tag(takes_context=True)
 def navbar_element(context, title, url_name):
     """
@@ -12,7 +34,31 @@ def navbar_element(context, title, url_name):
     is later reversed into proper URL.
     """
     url = reverse(url_name)
-    return navbar_element_url(context, title, url)
+    active = context['request'].path == url
+    return make_element(title, url, active=active)
+
+
+@register.simple_tag(takes_context=True)
+def navbar_element_permed(context, title, url_name, perms):
+    """
+    Works like `navbar_element`, but also disables if user doesn't have
+    permissions.
+
+    `perms` can be a comma-separated string of permissions or a single
+    permission.
+    """
+    url = reverse(url_name)
+    active = context['request'].path == url
+    disabled = True
+
+    # check permissions
+    perms_ctx = context['perms']
+    perms = perms.split(',')
+    perms = map(lambda x: x in perms_ctx, perms)
+    if all(perms):
+        disabled = False
+
+    return make_element(title, url, active=active, disabled=disabled)
 
 
 @register.simple_tag(takes_context=True)
@@ -21,14 +67,5 @@ def navbar_element_url(context, title, url):
     Insert Bootstrap's `<li><a>...</a></li>` with specific classes and
     accessibility elements.  This tag takes a pre-made URL as an argument.
     """
-    request = context['request']
-
-    active = ""
-    screen_reader = ""
-
-    if request.path == url:
-        active = 'class="active"'
-        screen_reader = '<span class="sr-only">(active)</span>'
-    tmplt = '<li {0}><a href="{1}">{2} {3}</a></li>'
-
-    return tmplt.format(active, url, title, screen_reader)
+    active = context['request'].path == url
+    return make_element(title, url, active=active)
