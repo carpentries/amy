a88546bff04becc88562f18bba73d44b491e17be
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2015-08-31 20:14:34 +0200

    Enable admin site for managing group permissions
    This is just easier than adding a separate management for groups...

--- amy/urls.py
+++ amy/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
+    url(r'^workshops/admin/', include(admin.site.urls)),
     url(r'^workshops/', include('workshops.urls')),
     # url(r'^account/', include('django.contrib.auth.urls')),
 
