be80edbdce8e9218c4bc02812d3292cf808a2cb5
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2015-09-11 19:02:14 +0200

    Get rid of artificial country EU (European Union)
    None of the events was using it.

--- amy/settings.py
+++ amy/settings.py
@@ -206,7 +206,6 @@ LOGIN_URL = '/account/login/'
 
 # explicitely add European Union as a country
 COUNTRIES_OVERRIDE = {
-    'EU': _('European Union'),
     'GB': _('United Kingdom'),
     'US': _('United States'),
     'W3': _('Online'),
