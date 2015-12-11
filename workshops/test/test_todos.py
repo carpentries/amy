87fa0f8da7a6c184cf630dd6ceb91bcc5b02f68a
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2015-12-11 16:56:23 +0100

    Add filtering shortcuts to TodoItem.objects manager
    These shortcuts come handy when we have to find todos for specific user
    and in a specific timerange.

--- workshops/test/test_todos.py
+++ workshops/test/test_todos.py
@@ -1,8 +1,8 @@
-import datetime
+from datetime import date, timedelta
 
 from django.core.urlresolvers import reverse
 
-from ..models import Event, TodoItem
+from ..models import Event, TodoItem, TodoItemQuerySet
 from .base import TestBase
 
 
@@ -19,7 +19,7 @@ class TestTodoItemViews(TestBase):
         event."""
         event = Event.objects.filter(slug__endswith="-upcoming") \
                              .order_by("-pk")[0]
-        event.end = event.start + datetime.timedelta(days=2)
+        event.end = event.start + timedelta(days=2)
         event.save()
 
         # check if the event has 0 todos
@@ -48,7 +48,7 @@ class TestTodoItemViews(TestBase):
 
         todo = TodoItem.objects.create(
             event=event, completed=False, title="Test TODO1",
-            due=datetime.date.today(), additional="",
+            due=date.today(), additional="",
         )
 
         assert todo.completed is False
@@ -64,7 +64,7 @@ class TestTodoItemViews(TestBase):
 
         todo = TodoItem.objects.create(
             event=event, completed=True, title="Test TODO2",
-            due=datetime.date.today(), additional="",
+            due=date.today(), additional="",
         )
 
         assert todo.completed is True
@@ -80,7 +80,7 @@ class TestTodoItemViews(TestBase):
 
         todo = TodoItem.objects.create(
             event=event, completed=False, title="Test TODO3",
-            due=datetime.date.today(), additional="",
+            due=date.today(), additional="",
         )
 
         assert todo in event.todoitem_set.all()
@@ -95,7 +95,7 @@ class TestTodoItemViews(TestBase):
 
         todo = TodoItem.objects.create(
             event=event, completed=False, title="Test TODO4",
-            due=datetime.date.today(), additional="",
+            due=date.today(), additional="",
         )
 
         url, form = self._get_initial_form('todo_edit', todo.pk)
@@ -110,3 +110,48 @@ class TestTodoItemViews(TestBase):
 
         assert todo.title == "Test TODO4 - new title"
         assert todo.completed is True
+
+
+class TestTodoManager(TestBase):
+    """Test for TodoItemQuerySet as a manager."""
+
+    def setUp(self):
+        super()._setUpHosts()
+        e = Event.objects.create(slug='event-with-todos', host=self.host_alpha)
+        self.today = date(2015, 12, 31)
+        self.current = TodoItem.objects.create(event=e, title='Current',
+                                               due=date(2015, 12, 30))
+        self.next = TodoItem.objects.create(event=e, title='Next',
+                                            due=date(2016, 1, 5))
+        self.outside1 = TodoItem.objects.create(event=e, title='Outside1',
+                                                due=date(2015, 12, 27))
+        self.outside2 = TodoItem.objects.create(event=e, title='Outside2',
+                                                due=date(2016, 1, 11))
+
+    def test_current_week_dates(self):
+        """Ensure we calculate start and end of current week correctly."""
+        start, end = TodoItemQuerySet.current_week_dates(today=self.today)
+        self.assertEqual(start, date(2015, 12, 28))
+        self.assertEqual(end, date(2016, 1, 4))
+
+    def test_next_week_dates(self):
+        """Ensure we calculate start and end of next week correctly."""
+        start, end = TodoItemQuerySet.next_week_dates(today=self.today)
+        self.assertEqual(start, date(2016, 1, 4))
+        self.assertEqual(end, date(2016, 1, 11))
+
+    def test_todos_current_week(self):
+        """Ensure we get todos that are due this week."""
+        todos = TodoItem.objects.current_week(today=self.today)
+        self.assertEqual(set(todos), set([self.current]))
+
+    def test_todos_next_week(self):
+        """Ensure we get todos that are due next week."""
+        todos = TodoItem.objects.next_week(today=self.today)
+        self.assertEqual(set(todos), set([self.next]))
+
+    def test_todos_current(self):
+        """Ensure we get todos that are due this and next week, and aren't
+        completed."""
+        todos = TodoItem.objects.current(today=self.today)
+        self.assertEqual(set(todos), set([self.current, self.next]))
