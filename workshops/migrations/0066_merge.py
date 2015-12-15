becd19c66924b04335ed8ff60f9ad02febf49375
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2015-12-15 16:50:46 +0100

    Add merge migration

---
+++ workshops/migrations/0066_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('workshops', '0064_dc_instructor_badge'),
+        ('workshops', '0065_multiple_memberships'),
+    ]
+
+    operations = [
+    ]
