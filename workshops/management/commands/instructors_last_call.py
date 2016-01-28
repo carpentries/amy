31b3a7a117d11d4f9b37c81a0272dd449d2bbdbd
Greg Wilson <gvwilson@third-bit.com>
2016-01-28 16:38:51 -0500

    Addressing feedback

--- workshops/management/commands/instructors_last_call.py
+++ workshops/management/commands/instructors_last_call.py
@@ -14,12 +14,16 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         # Every learner who was at this event but isn't an instructor.
-        slug = options['event']
-        event = Event.objects.get(slug=slug)
-        learner = Role.objects.get(name='learner')
-        ttt = Tag.objects.get(name='TTT')
-        instructor_badges = Badge.objects.instructor_badges()
-        trainees = Task.objects.filter(event=event, role=learner).exclude(person__badges__in=instructor_badges)
+        try:
+            slug = options['event']
+            event = Event.objects.get(slug=slug)
+            learner = Role.objects.get(name='learner')
+            ttt = Tag.objects.get(name='TTT')
+            instructor_badges = Badge.objects.instructor_badges()
+            trainees = Task.objects.filter(event=event, role=learner).exclude(person__badges__in=instructor_badges)
+        except ObjectDoesNotExist as e:
+            print(str(e), file=sys.stderr)
+            sys.exit(1)
 
         # Report.
         people = list(set([(t.person.family, t.person.personal, t.person.email) for t in trainees]))
