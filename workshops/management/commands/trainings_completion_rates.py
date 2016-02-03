32dfe1a0a2fe4974fda729b89a8fa301e256fa50
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2016-02-03 14:00:07 +0100

    trainings_completion_rates: fix CSV generation

--- workshops/management/commands/trainings_completion_rates.py
+++ workshops/management/commands/trainings_completion_rates.py
@@ -1,7 +1,6 @@
 import csv
 
 from django.core.management.base import BaseCommand
-from django.template.loader import get_template
 
 from workshops.models import Badge, Event, Tag, Person, Role
 from workshops.util import universal_date_format
@@ -46,29 +45,37 @@ class Command(BaseCommand):
         return 0.0
 
     def handle(self, *args, **options):
-        records = [['start', 'slug', 'online', 'badge', 'learners',
-                    'completed this', 'completed this %age',
-                    'completed other', 'completed other %age',
-                    'no badge', 'no badge %']]
+        fields = [
+            'start', 'slug', 'online', 'badge', 'learners',
+            'completed this', 'completed this [%]',
+            'completed other', 'completed other [%]',
+            'no badge', 'no badge [%]',
+        ]
+        writer = csv.DictWriter(self.stdout, fieldnames=fields)
+        writer.writeheader()
+
         for training in self.trainings():
             badge = self.badge_type(training.tags.all())
             learners = self.learners(training)
             learners_len = learners.count()
-            completed_len = \
-                learners.filter(badges=badge, award__event=training).count()
-            completed_other_len = \
-                learners.filter(badges=badge).exclude(award__event=training).count()
-            no_badge_len = \
-                learners.exclude(badges=badge).count()
-            records.append([universal_date_format(training.start),
-                            training.slug,
-                            int(self.online_tag in training.tags.all()),
-                            badge.title,
-                            learners_len,
-                            completed_len,
-                            self.percent(completed_len, learners_len),
-                            completed_other_len,
-                            self.percent(completed_other_len, learners_len),
-                            no_badge_len,
-                            self.percent(no_badge_len, learners_len)])
-        csv.writer(self.stdout).writerows(records)
+            completed_len = learners.filter(badges=badge,
+                                            award__event=training).count()
+            completed_other_len = learners.filter(badges=badge) \
+                                          .exclude(award__event=training) \
+                                          .count()
+            no_badge_len = learners.exclude(badges=badge).count()
+
+            record = {
+                fields[0]: universal_date_format(training.start),
+                fields[1]: training.slug,
+                fields[2]: int(self.online_tag in training.tags.all()),
+                fields[3]: badge.title,
+                fields[4]: learners_len,
+                fields[5]: completed_len,
+                fields[6]: self.percent(completed_len, learners_len),
+                fields[7]: completed_other_len,
+                fields[8]: self.percent(completed_other_len, learners_len),
+                fields[9]: no_badge_len,
+                fields[10]: self.percent(no_badge_len, learners_len),
+            }
+            writer.writerow(record)
