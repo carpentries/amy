6f45d74c27d44ee78c2aedb8590e451f121c6725
Shubham Singh <shubh.112358@gmail.com>
2016-03-04 02:55:58 +0530

    added hackathon tags

---
+++ workshops/migrations/0082_add_hackathon_tags.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+def add_hackathon_tags(apps, schema_editor):
+    """Add "hackathon tags."""
+    Tag = apps.get_model('workshops', 'Tag')
+    Tag.objects.create(
+        name='hackathon',
+        details='Event is a hackathon',
+    )
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('workshops', '0081_profileupdaterequest_notes'),
+    ]
+
+    operations = [
+        migrations.RunPython(add_hackathon_tags),
+        migrations.AlterField(
+            model_name='event',
+            name='tags',
+            field=models.ManyToManyField(to='workshops.Tag'),
+        ),
+    ]
