bd38b540b113ec570e56a0108d7931dbed3f0f45
Aditya Narayan <narayanaditya95@gmail.com>
2016-04-24 05:28:22 +0530

    Add a new `relation_diff` template tag. The relation_diff template tag returns the additions and deletions across a ForeignKey between two revesions - left and right. The additions and deletions are displayed as tags of different colours.

--- workshops/templatetags/diff.py
+++ workshops/templatetags/diff.py
@@ -9,3 +9,15 @@ register = template.Library()
 @register.simple_tag
 def semantic_diff(left, right, field):
     return mark_safe(generate_patch_html(left, right, field, cleanup='semantic'))
+
+
+@register.simple_tag
+def relation_diff(left, right, field):
+    left_relation = field.related_model.objects.get(pk=left.field_dict[field.name])
+    right_relation = field.related_model.objects.get(pk=right.field_dict[field.name])
+    if left_relation == right_relation:
+        return mark_safe('<div class="label label-default">{}</div>'.format(left_relation))
+    else:
+        deletion = '<div class="label label-danger">-{}</div>'.format(left_relation)
+        addition = '<div class="label label-success">+{}</div>'.format(right_relation)
+        return mark_safe(deletion + addition)
