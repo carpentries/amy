2f08bef5931676372d87b76039d79c31299c420b
Aditya Narayan <narayanaditya95@gmail.com>
2016-04-24 19:30:37 +0530

    Explicitly populate the addition, deletion and consistent relation list

--- workshops/templatetags/diff.py
+++ workshops/templatetags/diff.py
@@ -14,32 +14,43 @@ def semantic_diff(left, right, field):
 
 @register.simple_tag
 def relation_diff(left, right, field):
+    model = field.related_model
     if field.many_to_one or field.one_to_one:
         # {left,right}.field_dict[field.name] is an integer or does not exist
         # Cast it to a list(empty or single itemed)
         if left.field_dict.get(field.name):
-            left_PKs = [left.field_dict.get(field.name)]
+            try:
+                left_PKs = [model.objects.get(pk=left.field_dict.get(field.name))]
+            except model.DoesNotExist:
+                left_PKs = [left.field_dict.get(field.name)]
         else:
             left_PKs = []
         if right.field_dict.get(field.name):
-            right_PKs = [right.field_dict.get(field.name)]
+            try:
+                right_PKs = [model.objects.get(pk=right.field_dict.get(field.name))]
+            except model.DoesNotExist:
+                right_PKs = [right.field_dict.get(field.name)]
         else:
             right_PKs = []
     else:
-        left_PKs = left.field_dict.get(field.name,[])
-        right_PKs = right.field_dict.get(field.name,[])
+        left_PKs = []
+        for pk in left.field_dict.get(field.name, []):
+            try:
+                left_PKs.append(model.objects.get(pk=pk))
+            except model.DoesNotExist:
+                left_PKs.append(pk)
+        right_PKs = []
+        for pk in right.field_dict.get(field.name, []):
+            try:
+                right_PKs.append(model.objects.get(pk=pk))
+            except model.DoesNotExist:
+                right_PKs.append(pk)
     # Relations that exist only in the current version
-    additions = field.related_model.objects.filter(
-        ~Q(pk__in=left_PKs) & Q(pk__in=right_PKs)
-    )
+    additions = [obj for obj in right_PKs if obj not in left_PKs]
     # Relations that exist only in the previous version
-    deletions = field.related_model.objects.filter(
-        Q(pk__in=left_PKs) & ~Q(pk__in=right_PKs)
-    )
+    deletions = [obj for obj in left_PKs if obj not in right_PKs]
     # Relations that exist only in both versions
-    consistent = field.related_model.objects.filter(
-        Q(pk__in=left_PKs) & Q(pk__in=right_PKs)
-    )
+    consistent = [obj for obj in left_PKs if obj in right_PKs]
     add_label = ''.join('<a class="label label-success" href="{}">+{}</a>'.format(
             obj.get_absolute_url() if hasattr(obj, 'get_absolute_url') else '#',
             obj
