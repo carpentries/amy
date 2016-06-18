b0c5de958546ae2a658e4b6139af61ff4c005621
Aditya Narayan <narayanaditya95@gmail.com>
2016-06-18 16:23:08 +0530

    Create a new package `workshops.pydata` to hold PyData specific code.

---
+++ workshops/app_settings.py
@@ -0,0 +1,3 @@
+from django.conf import settings
+
+PYDATA_ENABLED = 'workshops.pydata' in settings.INSTALLED_APPS
---
+++ workshops/pydata/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+
+from .views import ConferenceImport
+
+
+urlpatterns = [
+]
