f78f8412d778124d808a86654c67205d24c6d2e6
Aditya Narayan <narayanaditya95@gmail.com>
2016-07-03 21:38:29 +0530

    Add a Sponsorship model. A sponsorship records the sponsorship from an organization to a particular event. Bundled required migrations. Fixes #830

--- workshops/models.py
+++ workshops/models.py
@@ -159,6 +159,33 @@ class Membership(models.Model):
 #------------------------------------------------------------
 
 
+class Sponsorship(models.Model):
+    '''Represent sponsorship from a host for an event.'''
+
+    organization = models.ForeignKey(
+        'Host',
+        on_delete=models.CASCADE,
+        help_text='Organization sponsoring the event'
+    )
+    event = models.ForeignKey(
+        'Event',
+        on_delete=models.CASCADE,
+    )
+    amount = models.DecimalField(
+        max_digits=8, decimal_places=2,
+        blank=True, null=True,
+        validators=[MinValueValidator(0)],
+        verbose_name='Sponsorship amount',
+        help_text='e.g. 1992.33'
+    )
+
+    def __str__(self):
+        return '{}: {}'.format(self.organization, self.amount)
+
+
+#------------------------------------------------------------
+
+
 @reversion.register
 class Airport(models.Model):
     '''Represent an airport (used to locate instructors).'''
