afe0ec1e30fe529623c292e9669ba6bffcf1b79e
Christopher Medrela <chris.medrela+github@gmail.com>
2016-08-03 19:52:11 +0200

    Move trainee dashboard tests to a new file
    From test_landing_page.py to test_trainee_dashboard.py.

--- workshops/test/test_landing_page.py
+++ workshops/test/test_landing_page.py
@@ -48,22 +48,6 @@ class TestAdminDashboard(TestBase):
         self.assertNotIn(completed, response.context['unpublished_events'])
 
 
-class TestTraineeDashboard(TestBase):
-    """ Tests for trainee dashboard. """
-    def setUp(self):
-        self.user = Person.objects.create_user(
-            username='user', personal='', family='',
-            email='user@example.org', password='pass')
-        self.client.login(username='user', password='pass')
-
-    def test_dashboard_loads(self):
-        rv = self.client.get(reverse('trainee-dashboard'))
-        self.assertEqual(rv.status_code, 200)
-        content = rv.content.decode('utf-8')
-        self.assertIn("Log out", content)
-        self.assertIn("Update your profile", content)
-
-
 class TestDispatch(TestBase):
     """ Test that the right dashboard (trainee or admin dashboard) is displayed
     after logging in."""
---
+++ workshops/test/test_trainee_dashboard.py
@@ -0,0 +1,20 @@
+from django.core.urlresolvers import reverse
+
+from workshops.models import Person
+from workshops.test import TestBase
+
+
+class TestTraineeDashboard(TestBase):
+    """ Tests for trainee dashboard. """
+    def setUp(self):
+        self.user = Person.objects.create_user(
+            username='user', personal='', family='',
+            email='user@example.org', password='pass')
+        self.client.login(username='user', password='pass')
+
+    def test_dashboard_loads(self):
+        rv = self.client.get(reverse('trainee-dashboard'))
+        self.assertEqual(rv.status_code, 200)
+        content = rv.content.decode('utf-8')
+        self.assertIn("Log out", content)
+        self.assertIn("Update your profile", content)
