46e2390804826c996e1a1a23fe68762a0cbe1653
Aditya Narayan <narayanaditya95@gmail.com>
2016-09-03 00:41:21 +0530

    Add `url` to unique_together constraint on Task model
    Bundle corresponding migration

--- workshops/models.py
+++ workshops/models.py
@@ -1508,16 +1508,13 @@ class Task(models.Model):
     event      = models.ForeignKey(Event)
     person     = models.ForeignKey(Person)
     role       = models.ForeignKey(Role)
-    title      = models.CharField(
-        max_length=STR_LONG,
-        blank=True,
-    )
+    title      = models.CharField(max_length=STR_LONG, blank=True)
     url        = models.URLField(blank=True)
 
     objects = TaskManager()
 
     class Meta:
-        unique_together = ("event", "person", "role")
+        unique_together = ('event', 'person', 'role', 'url')
         ordering = ("role__name", "event")
 
     def __str__(self):
