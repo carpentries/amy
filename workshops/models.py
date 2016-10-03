2c8bd0a6c681584295ed775689553299143f5342
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2016-10-03 16:03:04 +0200

    Training request: provide a meaningful str representation

--- workshops/models.py
+++ workshops/models.py
@@ -2052,7 +2052,16 @@ class TrainingRequest(ActiveMixin, CreatedUpdatedMixin, models.Model):
         return reverse('trainingrequest_details', args=[self.pk])
 
     def __str__(self):
-        return "Training request #{}".format(self.pk)
+        return (
+            '{state} training request by {personal} {family} <{email}>'
+            .format(
+                state=self.get_state_display(),
+                personal=self.personal,
+                family=self.family,
+                email=self.email,
+                timestamp=self.created_at,
+            )
+        )
 
 
 @reversion.register
