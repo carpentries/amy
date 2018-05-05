b31be1af0ac1b77537066b4eee8dd1be28a2a077
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2018-05-05 18:18:45 +0200

    Redirect `/` to `/workshops`
    This fixes #1197 and supersedes #1221.

--- amy/urls.py
+++ amy/urls.py
@@ -2,8 +2,10 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(pattern_name='dispatch')),
     url(r'^workshops/admin/', include(admin.site.urls)),
 ]
 if settings.ENABLE_PYDATA:
