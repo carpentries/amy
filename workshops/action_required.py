0a5199f1000ce97b0a0b16dadb98abf6b41425f2
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2018-05-24 22:31:23 +0200

    Add more URLs allowed to omit action-required redirection

--- workshops/action_required.py
+++ workshops/action_required.py
@@ -8,11 +8,16 @@ class PrivacyPolicy:
     def __call__(self, request):
         url = reverse('action_required_privacy')
 
+        allowed_urls = [
+            reverse('logout'),
+            url
+        ]
+
         # redirect only users who didn't agree on the privacy policy
         # also don't redirect if the requested page is the page we want to
         # redirect to
         if (
-                request.path != url and
+                request.path not in allowed_urls and
                 not request.user.is_anonymous and
                 hasattr(request.user, 'data_privacy_agreement') and
                 not request.user.data_privacy_agreement
