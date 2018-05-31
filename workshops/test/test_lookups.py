bf57f32cf3b526ef4a87dec0c2924f5e5f194c43
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2018-05-31 17:10:30 +0200

    Fix language lookup for profile update request form
    Bug: profile update request is available for non-authorized users,
    but it uses language lookup that was accessible only to logged in users.
    This commit fixes the error.

--- workshops/test/test_lookups.py
+++ workshops/test/test_lookups.py
@@ -7,13 +7,22 @@ from ..lookups import urlpatterns
 class TestLookups(TestBase):
     """Test suite for Django-Autocomplete-Light lookups."""
 
+    def setUp(self):
+        # prepare urlpatterns; only include lookup views that are restricted
+        # to logged-in users and/or admins
+        self.patterns_nonrestricted = ('language-lookup', )
+        self.urlpatterns = filter(
+            lambda pattern: pattern.name not in self.patterns_nonrestricted,
+            urlpatterns
+        )
+
     def test_login_regression(self):
         """Make sure lookups are login-protected"""
-        for pattern in urlpatterns:
+        for pattern in self.urlpatterns:
             rv = self.client.get(reverse(pattern.name))
             self.assertEqual(rv.status_code, 403, pattern.name)  # forbidden
 
         self._setUpUsersAndLogin()
-        for pattern in urlpatterns:
+        for pattern in self.urlpatterns:
             rv = self.client.get(reverse(pattern.name))
             self.assertEqual(rv.status_code, 200, pattern.name)  # OK
