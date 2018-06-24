67c8e8cb102ab6ce049228ed74327d200987f84d
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2018-06-24 10:47:31 +0200

    Add `NullableGithubUsernameField` for Github username fields in models
    This includes tests against that field.

---
+++ workshops/fields.py
@@ -0,0 +1,28 @@
+from django.core.validators import RegexValidator, MaxLengthValidator
+from django.db import models
+
+
+class NullableGithubUsernameField(models.CharField):
+    def __init__(self, **kwargs):
+        kwargs.setdefault('null', True)
+        kwargs.setdefault('blank', True)
+        kwargs.setdefault('default', '')
+        # max length of the GH username is 39 characters
+        kwargs.setdefault('max_length', 39)
+        super().__init__(**kwargs)
+
+    default_validators = [
+        MaxLengthValidator(
+            39,
+            message='Maximum allowed username length is 39 characters.',
+        ),
+        # according to https://stackoverflow.com/q/30281026,
+        # GH username can only contain alphanumeric characters and
+        # hyphens (but not consecutive), cannot start or end with
+        # a hyphen, and can't be longer than 39 characters
+        RegexValidator(
+            # regex inspired by above StackOverflow thread
+            regex=r'^([a-zA-Z\d](?:-?[a-zA-Z\d])*)$',
+            message='This is not a valid GitHub username.',
+        ),
+    ]
---
+++ workshops/test/test_fields.py
@@ -0,0 +1,37 @@
+from django.core.exceptions import ValidationError
+
+from .base import TestBase
+from ..fields import NullableGithubUsernameField
+
+
+class TestNullableGHUsernameField(TestBase):
+    def setUp(self):
+        self.passing = [
+            'harrypotter',
+            'hp',
+            'hpotter',
+            'harry-potter',
+            'harry-potter123',
+            'Harry-Potter',
+        ]
+
+        self.failing = [
+            'harry_potter',
+            'harry--potter',
+            '-harry',
+            'potter-',
+            'harry-averyveryverylongmiddlename-potter',
+        ]
+
+        self.field = NullableGithubUsernameField()
+
+    def test_passing_usernames(self):
+        """All correct usernames pass the field validation."""
+        for username in self.passing:
+            self.field.run_validators(username)
+
+    def test_failing_usernames(self):
+        """All incorrect usernames don't pass the field validation."""
+        for username in self.failing:
+            with self.assertRaises(ValidationError):
+                self.field.run_validators(username)
