07ded342a3dfd021d55cf53e1eccb08a392d429c
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2018-07-23 16:14:49 +0200

    Replace Bower with Yarn
    Bower is discontinued; Bower team suggests switching to Yarn or `npm`.
    
    Additionally, this commit:
    1. bumps up some of the required packages
    2. replaces Bootstrap3 with Bootstrap v4.1 (this change is pretty big
       and refreshes the UX)
    
    Fixes #1304.

--- amy/settings.py
+++ amy/settings.py
@@ -298,7 +298,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'bower_components'),
+    os.path.join(BASE_DIR, 'node_modules'),
 )
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
