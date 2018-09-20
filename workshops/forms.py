9fbd7aa33e699ab314833abba25f905147211fe4
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2018-09-20 18:47:09 +0200

    TrainingRequestUpdateForm: Disable `score_auto` field
    Admins won't be able to alter field's score calculated by AMY.

--- workshops/forms.py
+++ workshops/forms.py
@@ -1362,6 +1362,12 @@ class TrainingRequestUpdateForm(forms.ModelForm):
         widget=ModelSelect2(url='person-lookup')
     )
 
+    score_auto = forms.IntegerField(
+        disabled=True,
+        label=TrainingRequest._meta.get_field('score_auto').verbose_name,
+        help_text=TrainingRequest._meta.get_field('score_auto').help_text,
+    )
+
     helper = BootstrapHelper(duplicate_buttons_on_top=True,
                              submit_label='Update')
 
