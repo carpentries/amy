3230ec44cfa73c2d846e997e95d3d976a0ef0093
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2018-10-27 22:33:42 +0200

    Deprecate EventRequests, DCSelfOrganizedEventRequests
    This gets rid of the external form templates, views, URL, and
    changes basic navigation for logged-in users (adds "deprecated" badge
    to deprecated links).

--- extforms/views.py
+++ extforms/views.py
@@ -6,22 +6,12 @@ from django.urls import reverse_lazy
 from django.views.generic import TemplateView, RedirectView
 
 from workshops.forms import (
-    SWCEventRequestForm,
-    DCEventRequestForm,
-    EventSubmitForm,
-    DCSelfOrganizedEventRequestForm,
     TrainingRequestForm,
-    ProfileUpdateRequestForm,
 )
 from workshops.models import (
-    EventRequest,
-    EventSubmission as EventSubmissionModel,
-    DCSelfOrganizedEventRequest as DCSelfOrganizedEventRequestModel,
     TrainingRequest,
-    ProfileUpdateRequest,
 )
 from workshops.util import (
-    login_not_required,
     LoginNotRequiredMixin,
 )
 from workshops.base_views import (
@@ -31,160 +21,8 @@ from workshops.base_views import (
 )
 
 
-class SWCEventRequest(LoginNotRequiredMixin, EmailSendMixin, AMYCreateView):
-    model = EventRequest
-    form_class = SWCEventRequestForm
-    page_title = 'Request a Software Carpentry Workshop'
-    template_name = 'forms/workshop_swc_request.html'
-    success_url = reverse_lazy('swc_workshop_request_confirm')
-    email_fail_silently = False
-    email_kwargs = {
-        'to': settings.REQUEST_NOTIFICATIONS_RECIPIENTS,
-        'reply_to': None,
-    }
-
-    def get_success_message(self, *args, **kwargs):
-        """Don't display a success message."""
-        return ''
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = self.page_title
-        return context
-
-    def get_subject(self):
-        subject = (
-            '[{tag}] New workshop request: {affiliation}, {country}'
-        ).format(
-            tag=self.object.workshop_type.upper(),
-            country=self.object.country.name,
-            affiliation=self.object.affiliation,
-        )
-        return subject
-
-    def get_body(self):
-        link = self.object.get_absolute_url()
-        link_domain = settings.SITE_URL
-
-        body_txt = get_template(
-            'mailing/eventrequest.txt'
-        ).render({
-            'object': self.object,
-            'link': link,
-            'link_domain': link_domain,
-        })
-
-        body_html = get_template(
-            'mailing/eventrequest.html'
-        ).render({
-            'object': self.object,
-            'link': link,
-            'link_domain': link_domain,
-        })
-        return body_txt, body_html
-
-    def form_valid(self, form):
-        """Send email to admins if the form is valid."""
-        data = form.cleaned_data
-        self.email_kwargs['reply_to'] = (data['email'], )
-        result = super().form_valid(form)
-        return result
-
-
-class SWCEventRequestConfirm(LoginNotRequiredMixin, TemplateView):
-    """Display confirmation of received workshop request."""
-    template_name = 'forms/workshop_swc_request_confirm.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Thank you for requesting a workshop'
-        return context
-
-
-class DCEventRequest(SWCEventRequest):
-    form_class = DCEventRequestForm
-    page_title = 'Request a Data Carpentry Workshop'
-    template_name = 'forms/workshop_dc_request.html'
-    success_url = reverse_lazy('dc_workshop_request_confirm')
-
-
-class DCEventRequestConfirm(SWCEventRequestConfirm):
-    """Display confirmation of received workshop request."""
-    template_name = 'forms/workshop_dc_request_confirm.html'
-
-
-# This form is disabled
-class ProfileUpdateRequestView(LoginNotRequiredMixin, RedirectView):
-    permanent = False
-    query_string = False
-    url = 'https://static.carpentries.org/instructors/'
-
-
-# This form is disabled
-class EventSubmission(LoginNotRequiredMixin, TemplateView):
-    """Display form for submitting existing workshops."""
-    template_name = 'forms/event_submit.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Tell us about your workshop'
-        return context
-
-
-class DCSelfOrganizedEventRequest(LoginNotRequiredMixin, EmailSendMixin,
-                                  AMYCreateView):
-    "Display form for requesting self-organized workshops for Data Carpentry."
-    model = DCSelfOrganizedEventRequestModel
-    form_class = DCSelfOrganizedEventRequestForm
-    # we're reusing DC templates for normal workshop requests
-    template_name = 'forms/workshop_dc_request.html'
-    success_url = reverse_lazy('dc_workshop_selforganized_request_confirm')
-    email_fail_silently = False
-    email_kwargs = {
-        'to': settings.REQUEST_NOTIFICATIONS_RECIPIENTS,
-    }
-
-    def get_success_message(self, *args, **kwargs):
-        """Don't display a success message."""
-        return ''
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Register a self-organized Data Carpentry workshop'
-        return context
-
-    def get_subject(self):
-        return ('DC: new self-organized workshop request from {} @ {}'
-                .format(self.object.name, self.object.organization))
-
-    def get_body(self):
-        link = self.object.get_absolute_url()
-        link_domain = settings.SITE_URL
-        body_txt = get_template('mailing/dc_self_organized.txt') \
-            .render({
-                'object': self.object,
-                'link': link,
-                'link_domain': link_domain,
-            })
-        body_html = get_template('mailing/dc_self_organized.html') \
-            .render({
-                'object': self.object,
-                'link': link,
-                'link_domain': link_domain,
-            })
-        return body_txt, body_html
-
-
-class DCSelfOrganizedEventRequestConfirm(LoginNotRequiredMixin, TemplateView):
-    """Display confirmation of a received self-organized workshop request."""
-    # we're reusing DC templates for normal workshop requests
-    template_name = 'forms/workshop_dc_request_confirm.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Thanks for your submission'
-        return context
-
+#------------------------------------------------------------
+# TrainingRequest views
 
 class TrainingRequestCreate(
     LoginNotRequiredMixin,
@@ -216,3 +54,41 @@ class TrainingRequestConfirm(LoginNotRequiredMixin, TemplateView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Thank you for applying for an instructor training'
         return context
+
+
+#------------------------------------------------------------
+# Deprecated views
+
+class RedirectToWorkshopRequest(LoginNotRequiredMixin, RedirectView):
+    """A single class for handling redirect to new, unified workshop request
+    form, which replaces all other event-request(-kinda) forms."""
+    permanent = False
+    query_string = False
+    url = reverse_lazy('workshop_request')
+
+
+# This form is disabled
+class SWCEventRequest(RedirectToWorkshopRequest):
+    pass
+
+
+# This form is disabled
+class DCEventRequest(RedirectToWorkshopRequest):
+    pass
+
+
+# This form is disabled
+class EventSubmission(RedirectToWorkshopRequest):
+    pass
+
+
+# This form is disabled
+class DCSelfOrganizedEventRequest(RedirectToWorkshopRequest):
+    pass
+
+
+# This form is disabled
+class ProfileUpdateRequestView(LoginNotRequiredMixin, RedirectView):
+    permanent = False
+    query_string = False
+    url = 'https://static.carpentries.org/instructors/'
