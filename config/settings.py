4bcc077de82af4be7134acd1efadaf58f8c242bc
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2018-11-12 10:44:21 +0100

    Move static files from workshops application to their own dir
    Also: dir name was changed to `static` (from `staticfiles`) to easy
    distinguish from dir that collects static files (`staticfiles`).

--- config/settings.py
+++ config/settings.py
@@ -264,7 +264,7 @@ STATIC_ROOT = str(ROOT_DIR('staticfiles'))
 STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [
-    str(APPS_DIR.path('staticfiles')),
+    str(APPS_DIR.path('static')),
     str(ROOT_DIR.path('node_modules')),
 ]
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
