cd56fc1cba6aeeffa8047c26c38a41941d6de606
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2018-12-28 22:01:06 +0100

    Add tests for comments appearing with Membership and Organization create forms

--- amy/fiscal/tests/test_organization.py
+++ amy/fiscal/tests/test_organization.py
@@ -1,5 +1,7 @@
 from django.urls import reverse
+from django_comments.models import Comment
 
+from fiscal.forms import OrganizationCreateForm
 from workshops.models import Organization, Event
 from workshops.tests.base import TestBase
 
@@ -49,3 +51,32 @@ class TestOrganization(TestBase):
         rv = self.client.post(url, data=data)
         # make sure we're not updating to good values
         assert rv.status_code == 200
+
+    def test_creating_event_with_no_comment(self):
+        """Ensure that no comment is added when OrganizationCreateForm without
+        comment content is saved."""
+        self.assertEqual(Comment.objects.count(), 0)
+        data = {
+            'fullname': 'Test Organization',
+            'domain': 'test.org',
+            'comment': '',
+        }
+        form = OrganizationCreateForm(data)
+        form.save()
+        self.assertEqual(Comment.objects.count(), 0)
+
+    def test_creating_event_with_comment(self):
+        """Ensure that a comment is added when OrganizationCreateForm with
+        comment content is saved."""
+        self.assertEqual(Comment.objects.count(), 0)
+        data = {
+            'fullname': 'Test Organization',
+            'domain': 'test.org',
+            'comment': 'This is a test comment.',
+        }
+        form = OrganizationCreateForm(data)
+        obj = form.save()
+        self.assertEqual(Comment.objects.count(), 1)
+        comment = Comment.objects.first()
+        self.assertEqual(comment.comment, 'This is a test comment.')
+        self.assertIn(comment, Comment.objects.for_model(obj))
