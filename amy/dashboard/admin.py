bee97479c32440019e9a801f01aae04da51949e1
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2019-02-17 17:37:32 +0100

    Add Continent model to define continent with countries
    Admin panel was extended to allow admins to change or add other
    continents (for example Central America) as they want.
    
    7 continents were created via migration, and countries available in
    Django Countries were assigned to them.

--- amy/dashboard/admin.py
+++ amy/dashboard/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Criterium
+from .models import Criterium, Continent
 
 
 class CriteriumAdmin(admin.ModelAdmin):
@@ -16,4 +16,18 @@ class CriteriumAdmin(admin.ModelAdmin):
     get_countries.admin_order_field = 'countries'
 
 
+class ContinentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'get_countries')
+
+    def get_countries(self, obj):
+        if len(obj.countries) > 10:
+            return ", ".join([x.name for x in obj.countries[:10]]) + "..."
+        else:
+            return ", ".join([x.name for x in obj.countries])
+
+    get_countries.short_description = 'Matching countries'
+    get_countries.admin_order_field = 'countries'
+
+
 admin.site.register(Criterium, CriteriumAdmin)
+admin.site.register(Continent, ContinentAdmin)
--- amy/dashboard/models.py
+++ amy/dashboard/models.py
@@ -26,3 +26,24 @@ class Criterium(models.Model):
     def __str__(self):
         return "Criterium {name} ({email})".format(name=self.name,
                                                    email=self.email)
+
+
+class Continent(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Name",
+        blank=False,
+        unique=True,
+    )
+    countries = CountryField(
+        multiple=True,
+        verbose_name="Countries in this continent"
+    )
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Continent'
+        verbose_name_plural = 'Continents'
+
+    def __str__(self):
+        return "Continent {name}".format(name=self.name)
