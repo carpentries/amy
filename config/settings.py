bc105415a5b9f9495d2fc9c5ef6de6f6e133f6cc
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2019-03-16 19:18:30 +0100

    Force CSS/JS compression even in development, switch CSS compression engine
    This fixes #1482 bug - it was required to change the CSS compression
    engine, because it was causing issues to the resulting CSS.
    
    Django-Compressor was changed to ON in development, because there were
    far too many issues on production (as compared to development) due to
    no sufficient testing of the CSS/JS compression results.

--- config/settings.py
+++ config/settings.py
@@ -284,9 +284,14 @@ STATICFILES_FINDERS = [
 # DJANGO-COMPRESSOR
 # -----------------------------------------------------------------------------
 # https://django-compressor.readthedocs.io/en/stable/settings/
+COMPRESS_ENABLED = True
+
 COMPRESS_CSS_FILTERS = [
     'compressor.filters.css_default.CssAbsoluteFilter',
-    'compressor.filters.cssmin.CSSCompressorFilter',
+    'compressor.filters.cssmin.rCSSMinFilter',
+]
+COMPRESS_JS_FILTERS = [
+    'compressor.filters.jsmin.JSMinFilter',
 ]
 
 # MEDIA
