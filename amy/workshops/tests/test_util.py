5d5ab337c5ad848563cc4131a67693577471f8a2
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2019-08-16 20:54:15 +0200

    Fix tests - reflected changes in WorkshopRequest

--- amy/workshops/tests/test_util.py
+++ amy/workshops/tests/test_util.py
@@ -1034,11 +1034,16 @@ class TestMatchingNotificationEmail(TestBase):
     def setUp(self):
         self.request = WorkshopRequest.objects.create(
             state="p", personal="Harry", family="Potter", email="h@potter.com",
-            institution_other_name="Hogwarts", location="Scotland",
-            country="GB", preferred_dates="", other_preferred_dates="soon",
+            institution_other_name="Hogwarts",
+            institution_other_URL='hogwarts.uk',
+            location="Scotland", country="GB",
+            preferred_dates=None, other_preferred_dates="soon",
             language=Language.objects.get(name='English'),
+            number_attendees='10-40',
             audience_description="Students of Hogwarts",
-            organization_type='self',
+            administrative_fee='waiver',
+            travel_expences_management='booked',
+            institution_restrictions='no_restrictions',
         )
 
     def test_default_criteria(self):
