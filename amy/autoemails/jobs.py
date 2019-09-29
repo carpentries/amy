6dc800b1ed399dcf782383f397c23e915b2a155e
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2019-09-29 22:22:02 +0200

    Sample PoC of a job sending email
    And a docstring explaining how to use RQ Scheduler.

--- amy/autoemails/jobs.py
+++ amy/autoemails/jobs.py
@@ -1,5 +1,15 @@
+"""
+How to use RQ Scheduler in order to schedule these jobs:
+>>> from datetime import timedelta
+>>> import django_rq
+>>> scheduler = django_rq.get_scheduler('default')
+>>> job = scheduler.enqueue_in(timedelta(minutes=1), blocking, 30)
+"""
+
 import random
 import time
+
+from django.core import mail
 
 
 def blocking(t=60):
@@ -14,3 +24,16 @@ def fake_results(t=60):
     time.sleep(t)
     print(f"'fake_results' (t={t}) finished (results={r})")
     return r
+
+
+def send_test_email():
+    print(f"'send_test_email' started")
+    mail.send_mail('Subject here',
+                   'Long message',
+                   'from@example.com',
+                   ['to@example.org'],
+                   fail_silently=False)
+    print(f"'send_test_email' finished")
+    print("Testing the email outbox")
+    print(mail.outbox)
+    print('-' * 50)
