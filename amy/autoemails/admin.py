43e06fb2654260d4f4bf0fc5c6498d3d85088c37
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2019-09-30 21:25:23 +0200

    EmailTemplates: add 'queue' custom admin view for previewing the queue
    For now the queue is displayed in a very basic mode, and it should be
    extended soon.

--- amy/autoemails/admin.py
+++ amy/autoemails/admin.py
@@ -1,11 +1,35 @@
 from django.contrib import admin
+from django.template.response import TemplateResponse
+from django.urls import path
 
+import django_rq
 
 from .models import EmailTemplate
 
 
+scheduler = django_rq.get_scheduler('default')
+
+
 class EmailTemplateAdmin(admin.ModelAdmin):
     list_display = ['slug', 'subject', 'to_header', 'from_header']
+
+    def get_urls(self):
+        original_urls = super().get_urls()
+        new_urls = [
+            path('queue/', self.admin_site.admin_view(self.email_queue_view),
+                 name='autoemails_emailtemplate_queue'),
+        ]
+        return new_urls + original_urls
+
+    def email_queue_view(self, request):
+        jobs = scheduler.get_jobs()
+        context = dict(
+            self.admin_site.each_context(request),
+            title="Queue",
+            cl=self.get_changelist_instance(request),
+            queue=jobs,
+        )
+        return TemplateResponse(request, "queue.html", context)
 
 
 admin.site.register(EmailTemplate, EmailTemplateAdmin)
