6711ce3478ac5af06c12e294094f247bdd115ce4
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2019-12-08 12:44:05 +0100

    Autoemails admin: add RQJob email preview page

--- amy/autoemails/admin.py
+++ amy/autoemails/admin.py
@@ -3,6 +3,7 @@ from django.template.response import TemplateResponse
 from django.urls import path
 
 import django_rq
+from rq.job import Job
 
 from autoemails.models import EmailTemplate, Trigger, RQJob
 from workshops.util import admin_required
@@ -46,6 +47,41 @@ class TriggerAdmin(admin.ModelAdmin):
 
 class RQJobAdmin(admin.ModelAdmin):
     list_display = ['job_id', 'created_at', 'trigger']
+
+    def get_urls(self):
+        original_urls = super().get_urls()
+        new_urls = [
+            path(
+                '<path:object_id>/preview/',
+                admin_required(
+                    self.admin_site.admin_view(self.preview)
+                ),
+                name='autoemails_rqjob_preview',
+            ),
+        ]
+        return new_urls + original_urls
+
+    def preview(self, request, object_id):
+        rqjob = RQJob.objects.get(id=object_id)
+        job = Job.fetch(rqjob.job_id, connection=scheduler.connection)
+        instance = job.instance
+        trigger = instance.trigger
+        template = instance.template
+        email = instance._email()
+        adn_context = instance.context
+        context = dict(
+            self.admin_site.each_context(request),
+            title="Preview",
+            cl=self.get_changelist_instance(request),
+            rqjob=rqjob,
+            job=job,
+            instance=instance,
+            trigger=trigger,
+            template=template,
+            email=email,
+            adn_context=adn_context,
+        )
+        return TemplateResponse(request, "rqjob_preview.html", context)
 
 
 admin.site.register(EmailTemplate, EmailTemplateAdmin)
---
+++ amy/autoemails/templatetags/type_extras.py
@@ -0,0 +1,27 @@
+from collections.abc import Iterable
+
+from django import template
+from django.db.models import Model
+
+register = template.Library()
+
+
+@register.filter
+def get_type(value):
+    # inspired by: https://stackoverflow.com/a/12028864
+    return type(value)
+
+
+@register.filter
+def is_model(value):
+    return isinstance(value, Model)
+
+
+@register.filter
+def is_iterable(value):
+    return isinstance(value, Iterable)
+
+
+@register.filter
+def is_str(value):
+    return isinstance(value, str)
