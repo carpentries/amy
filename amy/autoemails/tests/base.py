f9b4f5857d3266a2c5661920144f33aad9ef8a3f
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2019-12-18 22:07:14 +0100

    Switch tests to fake redis implementation

--- amy/autoemails/tests/base.py
+++ amy/autoemails/tests/base.py
@@ -4,6 +4,9 @@ from redis import Redis
 from rq import Queue
 
 
+connection = FakeStrictRedis()
+
+
 class FakeRedisTestCaseMixin:
     """TestCase mixin that provides easy setup of FakeRedis connection to both
     Django-RQ and RQ-Scheduler, as well as test-teardown with scheduled jobs
@@ -11,8 +14,8 @@ class FakeRedisTestCaseMixin:
 
     def setUp(self):
         super().setUp()
-        # self.connection = FakeStrictRedis()
-        self.connection = Redis()
+        self.connection = connection
+        # self.connection = Redis()
         self.queue = Queue(is_async=False, connection=self.connection)
         self.scheduler = django_rq.get_scheduler('testing', queue=self.queue)
 
