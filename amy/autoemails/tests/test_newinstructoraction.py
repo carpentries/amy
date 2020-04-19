4d7c410be3ae1a8c484a805cd292c3c5ff383b05
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2020-04-19 11:38:43 +0200

    Reformat test_*action.py with Black

--- amy/autoemails/tests/test_newinstructoraction.py
+++ amy/autoemails/tests/test_newinstructoraction.py
@@ -9,36 +9,41 @@ from workshops.models import Task, Role, Person, Event, Tag, Organization
 
 class TestNewInstructorAction(TestCase):
     def setUp(self):
-        Tag.objects.bulk_create([
-            Tag(name='SWC'),
-            Tag(name='DC'),
-            Tag(name='LC'),
-            Tag(name='automated-email'),
-        ])
-        Organization.objects.bulk_create([
-            Organization(domain='librarycarpentry.org',
-                         fullname='Library Carpentry'),
-            Organization(domain='datacarpentry.org',
-                         fullname='Data Carpentry'),
-            Organization(domain='software-carpentry.org',
-                         fullname='Software Carpentry'),
-            Organization(domain='carpentries.org',
-                         fullname='Instructor Training'),
-        ])
+        Tag.objects.bulk_create(
+            [
+                Tag(name="SWC"),
+                Tag(name="DC"),
+                Tag(name="LC"),
+                Tag(name="automated-email"),
+            ]
+        )
+        Organization.objects.bulk_create(
+            [
+                Organization(
+                    domain="librarycarpentry.org", fullname="Library Carpentry"
+                ),
+                Organization(domain="datacarpentry.org", fullname="Data Carpentry"),
+                Organization(
+                    domain="software-carpentry.org", fullname="Software Carpentry"
+                ),
+                Organization(domain="carpentries.org", fullname="Instructor Training"),
+            ]
+        )
 
     def testLaunchAt(self):
         # the trigger and email template below are totally fake
         # and shouldn't pass validation
-        a = NewInstructorAction(trigger=Trigger(action='test-action',
-                                                template=EmailTemplate()))
+        a = NewInstructorAction(
+            trigger=Trigger(action="test-action", template=EmailTemplate())
+        )
         self.assertEqual(a.get_launch_at(), timedelta(hours=1))
 
     def testCheckConditions(self):
         """Make sure `check` works for various input data."""
         # totally fake Task, Role and Event data
-        LC_org = Organization.objects.get(domain='librarycarpentry.org')
+        LC_org = Organization.objects.get(domain="librarycarpentry.org")
         e = Event.objects.create(
-            slug='test-event',
+            slug="test-event",
             host=Organization.objects.first(),
             administrator=LC_org,
             start=date.today() + timedelta(days=7),
@@ -52,10 +57,9 @@ class TestNewInstructorAction(TestCase):
             # longitude=20.0,
             # url='https://test-event.example.com',
         )
-        e.tags.set(Tag.objects.filter(name__in=['SWC', 'DC', 'LC',
-                                                'automated-email']))
-        p = Person(personal='Harry', family='Potter', email='hp@magic.uk')
-        r = Role(name='instructor')
+        e.tags.set(Tag.objects.filter(name__in=["SWC", "DC", "LC", "automated-email"]))
+        p = Person(personal="Harry", family="Potter", email="hp@magic.uk")
+        r = Role(name="instructor")
         t = Task(event=e, person=p, role=r)
 
         # 1st case: everything is good
@@ -77,14 +81,14 @@ class TestNewInstructorAction(TestCase):
         self.assertEqual(NewInstructorAction.check(t), True)
 
         # 4th case: event is tagged with one (or more) excluding tags
-        e.tags.add(Tag.objects.get(name='cancelled'))
+        e.tags.add(Tag.objects.get(name="cancelled"))
         self.assertEqual(NewInstructorAction.check(t), False)
-        e.tags.remove(Tag.objects.get(name='cancelled'))
+        e.tags.remove(Tag.objects.get(name="cancelled"))
 
         # 5th case: role is different than 'instructor'
-        r.name = 'helper'
+        r.name = "helper"
         self.assertEqual(NewInstructorAction.check(t), False)
-        r.name = 'instructor'
+        r.name = "instructor"
 
         # 6th case: no administrator
         e.administrator = None
@@ -93,35 +97,35 @@ class TestNewInstructorAction(TestCase):
         e.administrator = LC_org
 
         # 7th case: wrong administrator (self organized or instructor training)
-        e.administrator = Organization.objects.get(domain='self-organized')
+        e.administrator = Organization.objects.get(domain="self-organized")
         e.save()
         self.assertEqual(NewInstructorAction.check(t), False)
-        e.administrator = Organization.objects.get(domain='carpentries.org')
+        e.administrator = Organization.objects.get(domain="carpentries.org")
         e.save()
         self.assertEqual(NewInstructorAction.check(t), False)
 
     def testCheckForNonContactablePerson(self):
         """Make sure `may_contact` doesn't impede `check()`."""
         # totally fake Task, Role and Event data
-        LC_org = Organization.objects.get(domain='librarycarpentry.org')
+        LC_org = Organization.objects.get(domain="librarycarpentry.org")
         e = Event.objects.create(
-            slug='test-event',
+            slug="test-event",
             host=Organization.objects.first(),
             administrator=LC_org,
             start=date.today() + timedelta(days=7),
             end=date.today() + timedelta(days=8),
-            country='GB',
-            venue='Ministry of Magic',
-            address='Underground',
+            country="GB",
+            venue="Ministry of Magic",
+            address="Underground",
             latitude=20.0,
             longitude=20.0,
-            url='https://test-event.example.com',
+            url="https://test-event.example.com",
         )
-        e.tags.set(Tag.objects.filter(name__in=['SWC', 'DC', 'LC',
-                                                'automated-email']))
-        p = Person(personal='Harry', family='Potter', email='hp@magic.uk',
-                   may_contact=True)  # contact allowed
-        r = Role(name='instructor')
+        e.tags.set(Tag.objects.filter(name__in=["SWC", "DC", "LC", "automated-email"]))
+        p = Person(
+            personal="Harry", family="Potter", email="hp@magic.uk", may_contact=True
+        )  # contact allowed
+        r = Role(name="instructor")
         t = Task(event=e, person=p, role=r)
         self.assertEqual(NewInstructorAction.check(t), True)
         p.may_contact = False  # contact disallowed
@@ -130,35 +134,37 @@ class TestNewInstructorAction(TestCase):
     def testContext(self):
         """Make sure `get_additional_context` works correctly."""
 
-        a = NewInstructorAction(trigger=Trigger(action='test-action',
-                                                template=EmailTemplate()))
+        a = NewInstructorAction(
+            trigger=Trigger(action="test-action", template=EmailTemplate())
+        )
 
         # method fails when obligatory objects are missing
         with self.assertRaises(KeyError):
             a.get_additional_context(dict())  # missing 'event'
         with self.assertRaises(KeyError):
-            a.get_additional_context(dict(event='dummy'))  # missing 'task'
+            a.get_additional_context(dict(event="dummy"))  # missing 'task'
         with self.assertRaises(AttributeError):
             # now both objects are present, but the method tries to execute
             # `refresh_from_db` on them
-            a.get_additional_context(dict(event='dummy', task='dummy'))
+            a.get_additional_context(dict(event="dummy", task="dummy"))
 
         e = Event.objects.create(
-            slug='test-event',
+            slug="test-event",
             host=Organization.objects.first(),
             start=date.today() + timedelta(days=7),
             end=date.today() + timedelta(days=8),
-            country='GB',
-            venue='Ministry of Magic',
-            address='Underground',
+            country="GB",
+            venue="Ministry of Magic",
+            address="Underground",
             latitude=20.0,
             longitude=20.0,
-            url='https://test-event.example.com',
+            url="https://test-event.example.com",
         )
-        e.tags.set(Tag.objects.filter(name='SWC'))
-        p = Person.objects.create(personal='Harry', family='Potter',
-                                  email='hp@magic.uk')
-        r = Role.objects.create(name='instructor')
+        e.tags.set(Tag.objects.filter(name="SWC"))
+        p = Person.objects.create(
+            personal="Harry", family="Potter", email="hp@magic.uk"
+        )
+        r = Role.objects.create(name="instructor")
         t = Task.objects.create(event=e, person=p, role=r)
 
         ctx = a.get_additional_context(objects=dict(event=e, task=t))
@@ -166,65 +172,68 @@ class TestNewInstructorAction(TestCase):
             ctx,
             dict(
                 workshop=e,
-                workshop_main_type='SWC',
+                workshop_main_type="SWC",
                 dates=e.human_readable_date,
                 host=Organization.objects.first(),
-                regional_coordinator_email=['admin-uk@carpentries.org'],
+                regional_coordinator_email=["admin-uk@carpentries.org"],
                 person=p,
                 instructor=p,
                 role=r,
                 task=t,
-                assignee='Regional Coordinator',
+                assignee="Regional Coordinator",
             ),
         )
 
     def test_event_slug(self):
         e = Event.objects.create(
-            slug='test-event',
+            slug="test-event",
             host=Organization.objects.first(),
             start=date.today() + timedelta(days=7),
             end=date.today() + timedelta(days=8),
-            country='GB',
-            venue='Ministry of Magic',
-            address='Underground',
+            country="GB",
+            venue="Ministry of Magic",
+            address="Underground",
             latitude=20.0,
             longitude=20.0,
-            url='https://test-event.example.com',
+            url="https://test-event.example.com",
         )
-        e.tags.set(Tag.objects.filter(name='SWC'))
-        p = Person.objects.create(personal='Harry', family='Potter',
-                                  email='hp@magic.uk')
-        r = Role.objects.create(name='instructor')
+        e.tags.set(Tag.objects.filter(name="SWC"))
+        p = Person.objects.create(
+            personal="Harry", family="Potter", email="hp@magic.uk"
+        )
+        r = Role.objects.create(name="instructor")
         t = Task.objects.create(event=e, person=p, role=r)
 
-        a = NewInstructorAction(trigger=Trigger(action='test-action',
-                                                template=EmailTemplate()),
-                                objects=dict(event=e, task=t))
+        a = NewInstructorAction(
+            trigger=Trigger(action="test-action", template=EmailTemplate()),
+            objects=dict(event=e, task=t),
+        )
 
         self.assertEqual(a.event_slug(), "test-event")
 
     def test_all_recipients(self):
         e = Event.objects.create(
-            slug='test-event',
+            slug="test-event",
             host=Organization.objects.first(),
             start=date.today() + timedelta(days=7),
             end=date.today() + timedelta(days=8),
-            country='GB',
-            venue='Ministry of Magic',
-            address='Underground',
+            country="GB",
+            venue="Ministry of Magic",
+            address="Underground",
             latitude=20.0,
             longitude=20.0,
-            url='https://test-event.example.com',
-
+            url="https://test-event.example.com",
         )
-        e.tags.set(Tag.objects.filter(name='SWC'))
-        p = Person.objects.create(personal='Harry', family='Potter',
-                                  email='hp@magic.uk')
-        r = Role.objects.create(name='instructor')
+        e.tags.set(Tag.objects.filter(name="SWC"))
+        p = Person.objects.create(
+            personal="Harry", family="Potter", email="hp@magic.uk"
+        )
+        r = Role.objects.create(name="instructor")
         t = Task.objects.create(event=e, person=p, role=r)
 
-        a = NewInstructorAction(trigger=Trigger(action='test-action',
-                                                template=EmailTemplate()),
-                                objects=dict(event=e, task=t))
+        a = NewInstructorAction(
+            trigger=Trigger(action="test-action", template=EmailTemplate()),
+            objects=dict(event=e, task=t),
+        )
 
         self.assertEqual(a.all_recipients(), "hp@magic.uk")
