13575531b8c69eae632190abde3f53d4f17f2f33
maneesha sane <maneeshasane@gmail.com>
2020-05-01 14:24:22 -0400

    update WRF text to ask about online workshops

--- amy/workshops/models.py
+++ amy/workshops/models.py
@@ -2550,7 +2550,11 @@ class WorkshopRequest(AssignmentMixin, StateMixin, CreatedUpdatedMixin,
     )
     user_notes = models.TextField(
         blank=True,
-        verbose_name="Is there anything else you would like to share with us?",
+        verbose_name="Will this workshop be conducted in-person or online? " 
+                     "Is there any other information you would like to share "
+                     "with us?",
+        help_text="Knowing if this workshop is on-line or in-person will "
+                  "help ensure we can best support you in coordinating the event."
     )
 
     class Meta:
