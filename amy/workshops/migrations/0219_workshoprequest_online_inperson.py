b667a117142485512f2633e85da4382ba97ef1ae
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2020-09-06 22:27:26 +0200

    Renaming migration to fix broken sequence

--- amy/workshops/migrations/0219_workshoprequest_online_inperson.py
+++ amy/workshops/migrations/0219_workshoprequest_online_inperson.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('workshops', '0217_auto_20200615_1436'),
+        ('workshops', '0218_membership_registration_code'),
     ]
 
     operations = [
