26b56410361a967e28148145ab7b8076013bfbdb
maneesha sane <maneeshasane@gmail.com>
2020-11-18 16:08:17 -0500

    add agreement link to member model

--- amy/workshops/models.py
+++ amy/workshops/models.py
@@ -162,6 +162,15 @@ class Membership(models.Model):
         "application.",
     )
 
+    agreement_link = models.URLField(
+        null=True,
+        blank=True,
+        default="",
+        verbose_name="Link to member agreement",
+        help_text="Link to member agreement document or folder in Google Drive",
+    )
+
+
     def __str__(self):
         from workshops.util import human_daterange
 
