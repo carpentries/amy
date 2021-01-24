1f24e30bc449f591f9aa23c5263cd19dafb752f9
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2021-01-24 20:25:39 +0100

    [1738] Search adjustments
    1. Search memberships by memberships registration code
    2. Easily find the only result
    3. When redirected to the only result, show notification.

--- amy/dashboard/views.py
+++ amy/dashboard/views.py
@@ -11,11 +11,11 @@ from django.db.models import (
     Q,
 )
 from django.shortcuts import render, redirect
+from django.utils.html import format_html
 from django.urls import reverse
 from django.views.decorators.http import require_GET
 from django_comments.models import Comment
 
-from workshops.forms import SearchForm
 from workshops.models import (
     Airport,
     Badge,
@@ -23,6 +23,7 @@ from workshops.models import (
     Qualification,
     Person,
     Organization,
+    Membership,
     Tag,
     TrainingRequest,
     TrainingProgress,
@@ -36,6 +37,7 @@ from workshops.util import (
 from dashboard.forms import (
     AutoUpdateProfileForm,
     SendHomeworkForm,
+    SearchForm,
 )
 
 
@@ -221,97 +223,114 @@ def search(request):
     """Search the database by term."""
 
     term = ""
-    organizations = events = persons = airports = training_requests = None
+    organizations = None
+    memberships = None
+    events = None
+    persons = None
+    airports = None
+    training_requests = None
     comments = None
+    only_result = None
 
     if request.method == "GET" and "term" in request.GET:
         form = SearchForm(request.GET)
         if form.is_valid():
-            term = form.cleaned_data["term"]
+            term = form.cleaned_data.get("term", "")
             tokens = re.split(r"\s+", term)
-            results = list()
 
-            if form.cleaned_data["in_organizations"]:
-                organizations = Organization.objects.filter(
-                    Q(domain__icontains=term) | Q(fullname__icontains=term)
-                ).order_by("fullname")
-                results += list(organizations)
+            organizations = Organization.objects.filter(
+                Q(domain__icontains=term) | Q(fullname__icontains=term)
+            ).order_by("fullname")
+            if len(organizations) == 1 and not only_result:
+                only_result = organizations[0]
 
-            if form.cleaned_data["in_events"]:
-                events = Event.objects.filter(
-                    Q(slug__icontains=term)
-                    | Q(host__domain__icontains=term)
-                    | Q(host__fullname__icontains=term)
-                    | Q(url__icontains=term)
-                    | Q(contact__icontains=term)
-                    | Q(venue__icontains=term)
-                    | Q(address__icontains=term)
-                ).order_by("-slug")
-                results += list(events)
+            memberships = Membership.objects.filter(
+                registration_code__icontains=term
+            ).order_by("-agreement_start")
+            if len(memberships) == 1 and not only_result:
+                only_result = memberships[0]
 
-            if form.cleaned_data["in_persons"]:
-                # if user searches for two words, assume they mean a person
-                # name
-                if len(tokens) == 2:
-                    name1, name2 = tokens
-                    complex_q = (
-                        (Q(personal__icontains=name1) & Q(family__icontains=name2))
-                        | (Q(personal__icontains=name2) & Q(family__icontains=name1))
-                        | Q(email__icontains=term)
-                        | Q(secondary_email__icontains=term)
-                        | Q(github__icontains=term)
-                    )
-                    persons = Person.objects.filter(complex_q)
-                else:
-                    persons = Person.objects.filter(
-                        Q(personal__icontains=term)
-                        | Q(family__icontains=term)
-                        | Q(email__icontains=term)
-                        | Q(secondary_email__icontains=term)
-                        | Q(github__icontains=term)
-                    ).order_by("family")
-                results += list(persons)
+            events = Event.objects.filter(
+                Q(slug__icontains=term)
+                | Q(host__domain__icontains=term)
+                | Q(host__fullname__icontains=term)
+                | Q(url__icontains=term)
+                | Q(contact__icontains=term)
+                | Q(venue__icontains=term)
+                | Q(address__icontains=term)
+            ).order_by("-slug")
+            if len(events) == 1 and not only_result:
+                only_result = events[0]
 
-            if form.cleaned_data["in_airports"]:
-                airports = Airport.objects.filter(
-                    Q(iata__icontains=term) | Q(fullname__icontains=term)
-                ).order_by("iata")
-                results += list(airports)
-
-            if form.cleaned_data["in_training_requests"]:
-                training_requests = TrainingRequest.objects.filter(
-                    Q(group_name__icontains=term)
+            # if user searches for two words, assume they mean a person
+            # name
+            if len(tokens) == 2:
+                name1, name2 = tokens
+                complex_q = (
+                    (Q(personal__icontains=name1) & Q(family__icontains=name2))
+                    | (Q(personal__icontains=name2) & Q(family__icontains=name1))
+                    | Q(email__icontains=term)
+                    | Q(secondary_email__icontains=term)
+                    | Q(github__icontains=term)
+                )
+                persons = Person.objects.filter(complex_q)
+            else:
+                persons = Person.objects.filter(
+                    Q(personal__icontains=term)
                     | Q(family__icontains=term)
                     | Q(email__icontains=term)
+                    | Q(secondary_email__icontains=term)
                     | Q(github__icontains=term)
-                    | Q(affiliation__icontains=term)
-                    | Q(location__icontains=term)
-                    | Q(user_notes__icontains=term)
-                )
-                results += list(training_requests)
+                ).order_by("family")
 
-            if form.cleaned_data["in_comments"]:
-                comments = Comment.objects.filter(
-                    Q(comment__icontains=term)
-                    | Q(user_name__icontains=term)
-                    | Q(user_email__icontains=term)
-                    | Q(user__personal__icontains=term)
-                    | Q(user__family__icontains=term)
-                    | Q(user__email__icontains=term)
-                    | Q(user__github__icontains=term)
-                ).prefetch_related("content_object")
-                results += list(comments)
+            if len(persons) == 1 and not only_result:
+                only_result = persons[0]
+
+            airports = Airport.objects.filter(
+                Q(iata__icontains=term) | Q(fullname__icontains=term)
+            ).order_by("iata")
+            if len(airports) == 1 and not only_result:
+                only_result = airports[0]
+
+            training_requests = TrainingRequest.objects.filter(
+                Q(group_name__icontains=term)
+                | Q(family__icontains=term)
+                | Q(email__icontains=term)
+                | Q(github__icontains=term)
+                | Q(affiliation__icontains=term)
+                | Q(location__icontains=term)
+                | Q(user_notes__icontains=term)
+            )
+            if len(training_requests) == 1 and not only_result:
+                only_result = training_requests[0]
+
+            comments = Comment.objects.filter(
+                Q(comment__icontains=term)
+                | Q(user_name__icontains=term)
+                | Q(user_email__icontains=term)
+                | Q(user__personal__icontains=term)
+                | Q(user__family__icontains=term)
+                | Q(user__email__icontains=term)
+                | Q(user__github__icontains=term)
+            ).prefetch_related("content_object")
+            if len(comments) == 1 and not only_result:
+                only_result = comments[0]
 
             # only 1 record found? Let's move to it immediately
-            if len(results) == 1:
-                result = results[0]
-                if isinstance(result, Comment):
+            if only_result:
+                msg = format_html(
+                    "You were moved to this page, because your search <i>{}</i> "
+                    "yields only this result.", term
+                )
+                if isinstance(only_result, Comment):
+                    messages.success(request, msg)
                     return redirect(
-                        result.content_object.get_absolute_url()
-                        + "#c{}".format(result.id)
+                        only_result.content_object.get_absolute_url()
+                        + "#c{}".format(only_result.id)
                     )
-                else:
-                    return redirect(result.get_absolute_url())
+                elif hasattr(only_result, "get_absolute_url"):
+                    messages.success(request, msg)
+                    return redirect(only_result.get_absolute_url())
 
         else:
             messages.error(request, "Fix errors below.")
@@ -324,11 +343,12 @@ def search(request):
         "title": "Search",
         "form": form,
         "term": term,
-        "organizations": organizations,
+        "organisations": organizations,
+        "memberships": memberships,
         "events": events,
         "persons": persons,
         "airports": airports,
         "comments": comments,
         "training_requests": training_requests,
     }
-    return render(request, "workshops/search.html", context)
+    return render(request, "dashboard/search.html", context)
