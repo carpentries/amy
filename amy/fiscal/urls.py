3a9698efa868941d8102685ae22bc832a356cbb7
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2021-02-14 13:47:00 +0100

    [#1774] Revert black formatting for urls

--- amy/fiscal/urls.py
+++ amy/fiscal/urls.py
@@ -1,76 +1,31 @@
+# flake8: noqa
 from django.urls import path, include
 
 from fiscal import views
 
 urlpatterns = [
     # organizations
-    path(
-        "organizations/",
-        include(
-            [
-                path("", views.AllOrganizations.as_view(), name="all_organizations"),
-                path(
-                    "add/", views.OrganizationCreate.as_view(), name="organization_add"
-                ),
-            ]
-        ),
-    ),
-    path(
-        "organization/<str:org_domain>/",
-        include(
-            [
-                path(
-                    "", views.OrganizationDetails.as_view(), name="organization_details"
-                ),
-                path(
-                    "edit/",
-                    views.OrganizationUpdate.as_view(),
-                    name="organization_edit",
-                ),
-                path(
-                    "delete/",
-                    views.OrganizationDelete.as_view(),
-                    name="organization_delete",
-                ),
-            ]
-        ),
-    ),
+    path("organizations/", include([
+        path("", views.AllOrganizations.as_view(), name="all_organizations"),
+        path("add/", views.OrganizationCreate.as_view(), name="organization_add"),
+    ])),
+    path("organization/<str:org_domain>/", include([
+        path("", views.OrganizationDetails.as_view(), name="organization_details"),
+        path("edit/", views.OrganizationUpdate.as_view(), name="organization_edit"),
+        path("delete/", views.OrganizationDelete.as_view(), name="organization_delete"),
+    ])),
     # memberships
-    path(
-        "memberships/",
-        include(
-            [
-                path("", views.AllMemberships.as_view(), name="all_memberships"),
-                path("add/", views.MembershipCreate.as_view(), name="membership_add"),
-            ]
-        ),
-    ),
-    path(
-        "membership/<int:membership_id>/",
-        include(
-            [
-                path("", views.MembershipDetails.as_view(), name="membership_details"),
-                path("edit/", views.MembershipUpdate.as_view(), name="membership_edit"),
-                path(
-                    "members/",
-                    views.MembershipMembers.as_view(),
-                    name="membership_members",
-                ),
-                path(
-                    "delete/",
-                    views.MembershipDelete.as_view(),
-                    name="membership_delete",
-                ),
-            ]
-        ),
-    ),
+    path("memberships/", include([
+        path("", views.AllMemberships.as_view(), name="all_memberships"),
+        path("add/", views.MembershipCreate.as_view(), name="membership_add"),
+    ])),
+    path("membership/<int:membership_id>/", include([
+        path("", views.MembershipDetails.as_view(), name="membership_details"),
+        path("edit/", views.MembershipUpdate.as_view(), name="membership_edit"),
+        path("members/", views.MembershipMembers.as_view(), name="membership_members"),
+        path("delete/", views.MembershipDelete.as_view(), name="membership_delete"),
+    ])),
     # sponsorships
-    path(
-        "sponsorships/add/", views.SponsorshipCreate.as_view(), name="sponsorship_add"
-    ),
-    path(
-        "sponsorship/<int:pk>/delete/",
-        views.SponsorshipDelete.as_view(),
-        name="sponsorship_delete",
-    ),
+    path("sponsorships/add/", views.SponsorshipCreate.as_view(), name="sponsorship_add"),
+    path("sponsorship/<int:pk>/delete/", views.SponsorshipDelete.as_view(), name="sponsorship_delete"),
 ]
