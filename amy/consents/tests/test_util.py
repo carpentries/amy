22f64cff0ae77ab81b29357cf63a01505efbf16a
Lauryn Brown <lauryndbrown@gmail.com>
2021-03-28 22:10:59 -0500

    Added test for util.py

---
+++ amy/consents/tests/test_util.py
@@ -0,0 +1,44 @@
+from django.test.testcases import TestCase
+from amy.consents.util import person_has_consented_to_required_terms
+from consents.models import Consent, Term, TermOption
+from django.utils import timezone
+from workshops.models import Person
+
+
+class TestActiveTermConsentsForm(TestCase):
+    def test_person_has_consented_to_required_terms(self) -> None:
+        # required term
+        required_term = Term.objects.create(
+            content="required_term",
+            slug="required_term",
+            required_type=Term.PROFILE_REQUIRE_TYPE,
+        )
+        required_term_option = TermOption.objects.create(
+            term=required_term, option_type=TermOption.AGREE
+        )
+        # optional Term
+        Term.objects.create(
+            content="not_required_term",
+            slug="not_required_term",
+            required_type=Term.OPTIONAL_REQUIRE_TYPE,
+        )
+        TermOption.objects.create(term=required_term, option_type=TermOption.AGREE)
+        # archived required term
+        Term.objects.create(
+            content="archived_term",
+            slug="archived_term",
+            archived_at=timezone.now(),
+            required_type=Term.PROFILE_REQUIRE_TYPE,
+        )
+        TermOption.objects.create(
+            term=required_term, option_type=TermOption.AGREE, archived_at=timezone.now()
+        )
+        person = Person.objects.create(
+            personal="Harry", family="Potter", email="hp@magic.uk"
+        )
+        self.assertEqual(person_has_consented_to_required_terms(person), False)
+        # Consent created for the required term; should return True
+        Consent.objects.create(
+            person=person, term=required_term, term_option=required_term_option
+        )
+        self.assertEqual(person_has_consented_to_required_terms(person), True)
--- amy/consents/util.py
+++ amy/consents/util.py
@@ -1,19 +1,5 @@
-from typing import Dict, Optional
-
 from consents.models import Consent, Term
 from workshops.models import Person
-
-
-def consent_by_term(person: Person) -> Dict[Term, Optional[Consent]]:
-    """
-    Returns a dictionary of all terms with an optional Consent
-    (null if the user has not consented, the consent object otherwise).
-    """
-    terms = Term.objects.active()
-    consents = Consent.objects.filter(person=person).active()
-    consent_by_term_id = {consent.term_id: consent for consent in consents}
-    consent_by_term = {term: consent_by_term_id.get(term.id) for term in terms}
-    return consent_by_term
 
 
 def person_has_consented_to_required_terms(person: Person) -> bool:
