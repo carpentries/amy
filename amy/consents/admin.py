d022ac213c1fbd442df78695744362458ecaf22d
Lauryn Brown <lauryndbrown@gmail.com>
2021-03-29 02:23:35 -0500

    Added admin view for consents

--- amy/consents/admin.py
+++ amy/consents/admin.py
@@ -1,3 +1,4 @@
+from consents.models import Consent
 from consents.models import Term, TermOption
 from django.contrib import admin
 
@@ -14,4 +15,18 @@ class TermAdmin(admin.ModelAdmin):
     ]
 
 
+class ConsentAdmin(admin.ModelAdmin):
+    list_display = ("person", "term", "term_option", "get_term_option_type")
+    readonly_fields = ("person", "term", "term_option", "archived_at")
+    search_fields = ("person__personal", "person__family", "person__email")
+    list_filter = ["term", "term_option__option_type", "archived_at"]
+
+    def get_term_option_type(self, obj):
+        return obj.term_option.option_type
+
+    get_term_option_type.short_description = "Term Option Type"
+    get_term_option_type.admin_order_field = "term_option__option_type"
+
+
 admin.site.register(Term, TermAdmin)
+admin.site.register(Consent, ConsentAdmin)
