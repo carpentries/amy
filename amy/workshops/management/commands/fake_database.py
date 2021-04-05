16944c20c50ac8ca7716b2d345de045368ab477c
Lauryn Brown <lauryndbrown@gmail.com>
2021-04-05 16:15:26 -0500

    fixed issue with fake_database

--- amy/workshops/management/commands/fake_database.py
+++ amy/workshops/management/commands/fake_database.py
@@ -990,7 +990,6 @@ class Command(BaseCommand):
                 public_profile,
             ],
         ).active().update(archived_at=timezone.now())
-
         Consent.objects.bulk_create(consents)
 
     def handle(self, *args, **options):
