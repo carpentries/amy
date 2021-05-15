8ebba5de25de289046bdca46f1613a337f1aacbf
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2021-05-15 14:28:38 +0200

    [#1944] Improve CommentForm tests

--- amy/extcomments/tests.py
+++ amy/extcomments/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 import django_comments
 
 from workshops.models import Organization, Person
@@ -9,26 +10,18 @@ class TestEmailFieldRequiredness(TestCase):
         """Regression test for #1944.
 
         Previously a user without email address would not be able to add a comment."""
-        # Arrange
-        person = Person.objects.create(
-            personal="Ron",
-            family="Weasley",
-            username="rw",
-            is_active=True,
-            email="",
-        )
-        person.set_password("testrwpassword")
-        self.client.login(username="rw", password="testrwpassword")
 
+        # Arrange
         organization = Organization.objects.create(
-            domain="example.org", fullname="Example Organisation"
+            domain="example.org",
+            fullname="Example Organisation",
         )
         CommentForm = django_comments.get_form()
 
         data = {
             "honeypot": "",
             "comment": "Content",
-            "name": "Ron",
+            "name": "Ron",  # required outside the request cycle
             **CommentForm(organization).generate_security_data(),
         }
 
@@ -37,3 +30,40 @@ class TestEmailFieldRequiredness(TestCase):
 
         # Assert
         self.assertTrue(form.is_valid())
+
+    def test_email_field_requiredness_POST(self):
+        """Regression test for #1944.
+
+        Previously a user without email address would not be able to add a comment.
+
+        This test makes a POST request with comment data."""
+
+        # Arrange
+        person = Person.objects.create(
+            personal="Ron",
+            family="Weasley",
+            username="rw",
+            is_active=True,
+            email="",
+            data_privacy_agreement=True,
+        )
+
+        organization = Organization.objects.create(
+            domain="example.org",
+            fullname="Example Organisation",
+        )
+
+        CommentModel = django_comments.get_model()
+        CommentForm = django_comments.get_form()
+        data = {
+            "honeypot": "",
+            "comment": "Content",
+            **CommentForm(organization).generate_security_data(),
+        }
+
+        # Act
+        self.client.force_login(person)
+        self.client.post(reverse("comments-post-comment"), data=data, follow=True)
+
+        # Assert
+        self.assertEqual(CommentModel.objects.for_model(organization).count(), 1)
