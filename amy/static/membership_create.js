bf193ef29b45a59be3ebb69b16a194f420f2d7b9
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2021-07-25 14:32:54 +0200

    [#1931] Set workshops, IT seats based on variant
    Change values in "workshops without admin fee" and "public instructor
    training seats" fields based on preconfigured configuration.
    
    Configuration depends on membership variant. When user changes selected
    variant to any of the supported ones, the field values will be changed.

--- amy/static/membership_create.js
+++ amy/static/membership_create.js
@@ -10,4 +10,30 @@ jQuery(function() {
             id_name.val(fullname);
         }
     });
+
+    $('#id_variant').on('change', (e) => {
+        const parameters = {
+            bronze: {
+                it_seats_public: 0,
+                workshops: 2
+            },
+            silver: {
+                it_seats_public: 6,
+                workshops: 4
+            },
+            gold: {
+                it_seats_public: 15,
+                workshops: 6
+            }
+        };
+
+        const publicInstructorTrainingSeats = $("#id_public_instructor_training_seats");
+        const workshopsWithoutAdminFee = $("#id_workshops_without_admin_fee_per_agreement");
+
+        if (e.target.value in parameters) {
+            // set values from parameters
+            publicInstructorTrainingSeats.val(parameters[e.target.value].it_seats_public);
+            workshopsWithoutAdminFee.val(parameters[e.target.value].workshops);
+        }
+    });
 });
