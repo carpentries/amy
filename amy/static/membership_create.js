180abd73a2017b8ea1668ba01219960aae0accf5
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2021-07-25 15:05:38 +0200

    [#1931] Format with default Prettier values

--- amy/static/membership_create.js
+++ amy/static/membership_create.js
@@ -1,57 +1,63 @@
-jQuery(function() {
-    $('#id_main_organization').on('change.select2', (e) => {
-        // additional data is sent from the backend and it's stored in
-        // select2 `data` parameter
-        const fullname = $(e.target).select2("data")[0].fullname;
-        const id_name = $("#id_name");
+jQuery(function () {
+  $("#id_main_organization").on("change.select2", (e) => {
+    // additional data is sent from the backend and it's stored in
+    // select2 `data` parameter
+    const fullname = $(e.target).select2("data")[0].fullname;
+    const id_name = $("#id_name");
 
-        // only write the new name if "name" field is empty
-        if (!!fullname && !!id_name && !id_name.val()) {
-            id_name.val(fullname);
-        }
-    });
+    // only write the new name if "name" field is empty
+    if (!!fullname && !!id_name && !id_name.val()) {
+      id_name.val(fullname);
+    }
+  });
 
-    // prepopulate some fields based on selected variant
-    const variantChange = (e) => {
-        const parameters = {
-            bronze: {
-                it_seats_public: 0,
-                workshops: 2
-            },
-            silver: {
-                it_seats_public: 6,
-                workshops: 4
-            },
-            gold: {
-                it_seats_public: 15,
-                workshops: 6
-            }
-        };
-
-        const publicInstructorTrainingSeats = $("#id_public_instructor_training_seats");
-        const workshopsWithoutAdminFee = $("#id_workshops_without_admin_fee_per_agreement");
-        const changedFieldValues = $("#standard_packages_prepopulation_warning");
-
-        const variant = e ? $(e.target) : $("#id_variant");
-
-        if (variant.val() in parameters) {
-            // set values from parameters
-            publicInstructorTrainingSeats.val(parameters[variant.val()].it_seats_public);
-            workshopsWithoutAdminFee.val(parameters[variant.val()].workshops);
-
-            // set visual indication
-            publicInstructorTrainingSeats.addClass('border-warning');
-            workshopsWithoutAdminFee.addClass('border-warning');
-            changedFieldValues.removeClass('d-none');
-        } else {
-            // remove visual indication (if any set)
-            publicInstructorTrainingSeats.removeClass('border-warning');
-            workshopsWithoutAdminFee.removeClass('border-warning');
-            changedFieldValues.addClass('d-none');
-        }
+  // prepopulate some fields based on selected variant
+  const variantChange = (e) => {
+    const parameters = {
+      bronze: {
+        it_seats_public: 0,
+        workshops: 2,
+      },
+      silver: {
+        it_seats_public: 6,
+        workshops: 4,
+      },
+      gold: {
+        it_seats_public: 15,
+        workshops: 6,
+      },
     };
-    variantChange();  // on load
-    $('#id_variant').on('change', (e) => {
-        variantChange(e);
-    });
+
+    const publicInstructorTrainingSeats = $(
+      "#id_public_instructor_training_seats"
+    );
+    const workshopsWithoutAdminFee = $(
+      "#id_workshops_without_admin_fee_per_agreement"
+    );
+    const changedFieldValues = $("#standard_packages_prepopulation_warning");
+
+    const variant = e ? $(e.target) : $("#id_variant");
+
+    if (variant.val() in parameters) {
+      // set values from parameters
+      publicInstructorTrainingSeats.val(
+        parameters[variant.val()].it_seats_public
+      );
+      workshopsWithoutAdminFee.val(parameters[variant.val()].workshops);
+
+      // set visual indication
+      publicInstructorTrainingSeats.addClass("border-warning");
+      workshopsWithoutAdminFee.addClass("border-warning");
+      changedFieldValues.removeClass("d-none");
+    } else {
+      // remove visual indication (if any set)
+      publicInstructorTrainingSeats.removeClass("border-warning");
+      workshopsWithoutAdminFee.removeClass("border-warning");
+      changedFieldValues.addClass("d-none");
+    }
+  };
+  variantChange(); // on load
+  $("#id_variant").on("change", (e) => {
+    variantChange(e);
+  });
 });
