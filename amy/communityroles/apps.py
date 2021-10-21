3976bf38db0ffe0715e51285c67acc3610def2d7
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2021-10-21 21:50:01 +0200

    [#2021] Add "communityroles" app

---
+++ amy/communityroles/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CommunityRolesConfig(AppConfig):
+    name = "communityroles"
--- config/settings.py
+++ config/settings.py
@@ -133,6 +133,7 @@ LOCAL_APPS = [
     "amy.extcomments.apps.ExtcommentsConfig",
     "amy.autoemails.apps.AutoemailsConfig",
     "amy.consents.apps.ConsentsConfig",
+    "amy.communityroles.apps.CommunityRolesConfig",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
