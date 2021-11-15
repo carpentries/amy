0414e36d37a4a65ccda921cb467085f00880008c
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2021-11-15 23:15:28 +0100

    [#2024] Fix black error in one docstring

--- amy/workshops/base_views.py
+++ amy/workshops/base_views.py
@@ -293,7 +293,7 @@ class AutoresponderMixin:
         return "email"
 
     def autoresponder_email_context(self, form):
-        """Context for """
+        """Context for"""
         # list of fields allowed to show to the user
         whitelist = []
         form_data = [v for k, v in form.cleaned_data.items() if k in whitelist]
