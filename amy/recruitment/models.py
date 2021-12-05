34092fc2a08faa6174309dd8a9227c4ed5c21c3f
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2021-12-05 17:35:25 +0100

    [#2061] Add basic models for Instructor Recruitment

---
+++ amy/recruitment/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from reversion import revisions as reversion
+
+from workshops.mixins import CreatedUpdatedMixin, StateMixin
+from workshops.models import Event, Person
+
+
+@reversion.register
+class InstructorRecruitment(CreatedUpdatedMixin, models.Model):
+    """Instructor recruitment process for a given event."""
+
+    STATUS_CHOICES = (
+        ("o", "Open"),
+        ("c", "Closed"),
+    )
+    status = models.CharField(
+        max_length=1, choices=STATUS_CHOICES, null=False, blank=False, default="o"
+    )
+    notes = models.TextField(default="", null=False, blank=True)
+    event = models.OneToOneField(
+        Event, on_delete=models.PROTECT, null=False, blank=False
+    )
+
+
+@reversion.register
+class InstructorRecruitmentSignup(CreatedUpdatedMixin, StateMixin, models.Model):
+    """Instructor signup for a given event instructor recruitment process."""
+
+    recruitment = models.ForeignKey(
+        InstructorRecruitment, on_delete=models.CASCADE, null=False, blank=False
+    )
+    person = models.ForeignKey(
+        Person, on_delete=models.CASCADE, null=False, blank=False
+    )
+
+    INTEREST_CHOICES = (
+        ("session", "Whole session"),
+        ("part", "Part of session"),
+        ("support", "Supporting instructor"),
+    )
+    interest = models.CharField(
+        max_length=10, choices=INTEREST_CHOICES, null=False, blank=False
+    )
+    user_notes = models.TextField(default="", null=False, blank=True)
+    notes = models.TextField(default="", null=False, blank=True)  # admin notes
