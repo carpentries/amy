c70113c1b6575859eac8d97d79e6c33aa146fdcb
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2022-01-22 17:18:06 +0100

    [#2063] Fix settings based on Django-Debug-Toolbar check W006
    https://django-debug-toolbar.readthedocs.io/en/latest/checks.html
    https://github.com/jazzband/django-debug-toolbar/issues/1550#issuecomment-995929674
    
    Based on the discussion in the linked thread, loader configuration was
    replaced with `"APP_DIRS": True`.
    
    Django adds required loaders by default, so there was
    no need to specify them directly.

--- config/settings.py
+++ config/settings.py
@@ -329,15 +329,10 @@ TEMPLATES = [
         "DIRS": [
             str(APPS_DIR.path("templates")),
         ],
+        "APP_DIRS": True,
         "OPTIONS": {
             # https://docs.djangoproject.com/en/dev/ref/settings/#template-debug
             "debug": DEBUG,
-            # https://docs.djangoproject.com/en/dev/ref/settings/#template-loaders
-            # https://docs.djangoproject.com/en/dev/ref/templates/api/#loader-types
-            "loaders": [
-                "django.template.loaders.filesystem.Loader",
-                "django.template.loaders.app_directories.Loader",
-            ],
             # https://docs.djangoproject.com/en/dev/ref/settings/#template-context-processors
             "context_processors": [
                 "django.template.context_processors.debug",
