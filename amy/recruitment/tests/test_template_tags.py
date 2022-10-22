7ff53834e757c2426fcfa86f2e9c2141188a6be0
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2022-10-22 15:21:10 +0200

    [#2249] Fix issues with empty event dates

--- amy/recruitment/tests/test_template_tags.py
+++ amy/recruitment/tests/test_template_tags.py
@@ -51,6 +51,24 @@ class TestInstructorRecruitmentTemplateTags(TestCase):
         # Assert
         self.assertEqual(results, [event2])
 
+    def test_get_event_conflicts_no_start_or_end_dates(self) -> None:
+        """Regression test for #2243."""
+        # Arrange
+        event1 = Event(
+            slug="2022-01-01-test", start=date(2022, 1, 1), end=date(2022, 1, 2)
+        )
+        event2 = Event(slug="2022-02-01-test", start=None, end=date(2022, 2, 2))
+        event3 = Event(slug="2022-03-01-test", start=date(2022, 3, 1), end=None)
+        event4 = Event(slug="2022-xx-xx-test", start=None, end=None)
+        events = [event1, event2, event3, event4]
+        event = Event(
+            slug="2022-01-02-test", start=date(2022, 1, 2), end=date(2022, 1, 3)
+        )
+        # Act
+        results = get_event_conflicts(events, event)
+        # Assert
+        self.assertEqual(results, [event1])
+
     def test_get_events_nearby(self) -> None:
         # Arrange
         event1 = Event(
@@ -70,6 +88,24 @@ class TestInstructorRecruitmentTemplateTags(TestCase):
         results = get_events_nearby(events, event, days_before=100)
         # Assert
         self.assertEqual(results, [event1, event2])
+
+    def test_get_events_nearby_no_start_or_end_dates(self) -> None:
+        """Regression test for #2243."""
+        # Arrange
+        event1 = Event(
+            slug="2022-01-01-test", start=date(2022, 1, 1), end=date(2022, 1, 2)
+        )
+        event2 = Event(slug="2022-02-01-test", start=None, end=date(2022, 2, 2))
+        event3 = Event(slug="2022-03-01-test", start=date(2022, 3, 1), end=None)
+        event4 = Event(slug="2022-xx-xx-test", start=None, end=None)
+        events = [event1, event2, event3, event4]
+        event = Event(
+            slug="2021-12-22-test", start=date(2021, 12, 22), end=date(2021, 12, 23)
+        )
+        # Act
+        results = get_events_nearby(events, event)
+        # Assert
+        self.assertEqual(results, [event1])
 
     def test_get_signup_conflicts(self) -> None:
         # Arrange
@@ -104,6 +140,42 @@ class TestInstructorRecruitmentTemplateTags(TestCase):
         results = get_signup_conflicts(signups, recruitment)
         # Assert
         self.assertEqual(results, [signup2])
+
+    def test_get_signup_conflicts_no_start_or_end_dates(self) -> None:
+        """Regression test for #2243."""
+        # Arrange
+        signup1 = InstructorRecruitmentSignup(
+            recruitment=InstructorRecruitment(
+                event=Event(
+                    slug="2022-01-01-test", start=date(2022, 1, 1), end=date(2022, 1, 2)
+                )
+            )
+        )
+        signup2 = InstructorRecruitmentSignup(
+            recruitment=InstructorRecruitment(
+                event=Event(slug="2022-02-01-test", start=None, end=date(2022, 2, 2))
+            )
+        )
+        signup3 = InstructorRecruitmentSignup(
+            recruitment=InstructorRecruitment(
+                event=Event(slug="2022-03-01-test", start=date(2022, 3, 1), end=None)
+            )
+        )
+        signup4 = InstructorRecruitmentSignup(
+            recruitment=InstructorRecruitment(
+                event=Event(slug="2022-xx-xx-test", start=None, end=None)
+            )
+        )
+        signups = [signup1, signup2, signup3, signup4]
+        recruitment = InstructorRecruitment(
+            event=Event(
+                slug="2022-01-02-test", start=date(2022, 1, 2), end=date(2022, 1, 3)
+            )
+        )
+        # Act
+        results = get_signup_conflicts(signups, recruitment)
+        # Assert
+        self.assertEqual(results, [signup1])
 
     def test_priority_label__success(self) -> None:
         # Arrange
