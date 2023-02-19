c4a3774a4d194c1fd190837240c265a34b100dc3
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2023-02-19 19:19:16 +0100

    [#2293] Util for setting consent to given value for given user

--- amy/consents/tests/test_util.py
+++ amy/consents/tests/test_util.py
@@ -1,8 +1,12 @@
+from django.test import TestCase
 from django.utils import timezone
 
-from consents.models import Term, TermOption, TermOptionChoices
+from consents.models import Consent, Term, TermEnum, TermOption, TermOptionChoices
 from consents.tests.base import ConsentTestBase
-from consents.util import person_has_consented_to_required_terms
+from consents.util import (
+    person_has_consented_to_required_terms,
+    reconsent_for_term_option_type,
+)
 from workshops.models import Person
 
 
@@ -38,3 +42,103 @@ class TestActiveTermConsentsForm(ConsentTestBase):
         # Consents created for the required terms; should return True
         self.person_agree_to_terms(person, required_terms)
         self.assertEqual(person_has_consented_to_required_terms(person), True)
+
+
+class TestReconsentForTermOptionType(TestCase):
+    def test_term_not_found(self) -> None:
+        # Arrange
+        term_key = "test"  # this is incompatible with TermEnum and is ignored below
+        term_option_type = TermOptionChoices.AGREE
+        person = Person()
+
+        # Act & Assert
+        with self.assertRaises(Term.DoesNotExist):
+            reconsent_for_term_option_type(
+                term_key,  # type: ignore
+                term_option_type,
+                person,
+            )
+
+    def test_term_option_not_found(self) -> None:
+        # Arrange
+        term_key = TermEnum.MAY_CONTACT
+        term_option_type = "test"  # incompatible with TermOptionChoices; ignored below
+        person = Person()
+
+        # Act & Assert
+        with self.assertRaises(TermOption.DoesNotExist):
+            reconsent_for_term_option_type(
+                term_key,
+                term_option_type,  # type: ignore
+                person,
+            )
+
+    def test_consent_not_found(self) -> None:
+        """Should not happen unless someone intentionally removes a Consent from DB."""
+        # Arrange
+        term_key = TermEnum.MAY_CONTACT
+        term_option_type = TermOptionChoices.AGREE
+        person = Person.objects.create(
+            personal="Harry", family="Potter", email="hp@magic.uk"
+        )
+        Consent.objects.filter(
+            term__slug=Term.key_to_slug(term_key), person=person
+        ).delete()
+
+        # Act & Assert
+        with self.assertRaises(Consent.DoesNotExist):
+            reconsent_for_term_option_type(
+                term_key,
+                term_option_type,
+                person,
+            )
+
+    def test_old_consent_is_archived(self) -> None:
+        # Arrange
+        term_key = TermEnum.MAY_CONTACT
+        term_option_type = TermOptionChoices.AGREE
+        person = Person.objects.create(
+            personal="Harry", family="Potter", email="hp@magic.uk"
+        )
+        old_consent = Consent.objects.get(
+            term__slug=Term.key_to_slug(term_key), person=person
+        )
+
+        # Act
+        new_consent = reconsent_for_term_option_type(
+            term_key,
+            term_option_type,
+            person,
+        )
+        old_consent.refresh_from_db()
+
+        # Assert
+        self.assertTrue(old_consent.is_archived())
+        self.assertTrue(new_consent.is_active())
+
+    def test_new_consent_created(self) -> None:
+        # Arrange
+        term_key = TermEnum.MAY_CONTACT
+        term_option_type = TermOptionChoices.AGREE
+        person = Person.objects.create(
+            personal="Harry", family="Potter", email="hp@magic.uk"
+        )
+        old_consent = Consent.objects.get(
+            term__slug=Term.key_to_slug(term_key), person=person
+        )
+
+        # Act
+        new_consent = reconsent_for_term_option_type(
+            term_key,
+            term_option_type,
+            person,
+        )
+
+        # Assert
+        self.assertNotEqual(old_consent.pk, new_consent.pk)
+        self.assertEqual(old_consent.term, new_consent.term)
+        self.assertIsNone(old_consent.term_option)
+        # New consent term option is actually a value from database, whereas the old
+        # consent was never set to anything but None (the default value).
+        self.assertNotEqual(old_consent.term_option, new_consent.term_option)
+        self.assertEqual(old_consent.person, new_consent.person)
