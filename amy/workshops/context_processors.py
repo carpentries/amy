6d031cebf45a2984fb48480f19918c2d40c3cf01
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2023-03-04 16:51:06 +0100

    [#2322] Add SITE_BANNER_STYLE from settings to all context processors

--- amy/workshops/context_processors.py
+++ amy/workshops/context_processors.py
@@ -1,8 +1,14 @@
 from django.http import HttpRequest
+from django.conf import settings
 
 from amy import __version__
 
 
 def version(request: HttpRequest) -> dict:
     data = {"amy_version": __version__}
+    return data
+
+
+def site_banner(request: HttpRequest) -> dict:
+    data = {"SITE_BANNER_STYLE": settings.SITE_BANNER_STYLE}
     return data
