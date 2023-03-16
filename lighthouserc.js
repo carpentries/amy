80f8fe084ad023b2108e1f98185f20f261bbc8f3
Eli Chadwick <elichadwick@carpentries.org>
2023-03-16 16:44:13 +0000

    add puppeteer script and set up dev database

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,8 +1,9 @@
 module.exports = {
     ci: {
         collect: {
-            url: ['http://localhost:8000/'],
+            url: ['http://127.0.0.1:8000/'],
             startServerCommand: 'pipenv run make serve',
+            puppeteerScript: 'puppeteer-script.js'
         },
         upload: {
             target: 'temporary-public-storage',
