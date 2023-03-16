0039f81aed3abbb809d3c3ae2c1eca434240d193
Eli Chadwick <elichadwick@carpentries.org>
2023-03-16 17:35:43 +0000

    try to get chromium v110

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
     ci: {
         collect: {
-            url: ['http://localhost:8000/'],
+            url: ['http://127.0.0.1:8000/'],
             startServerCommand: 'pipenv run make serve',
             puppeteerScript: 'puppeteer-script.js'
         },
