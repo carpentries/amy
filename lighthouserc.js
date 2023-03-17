bc7f7507d63bfd06369d41d0fedf9d6ca6425911
Eli Chadwick <elichadwick@carpentries.org>
2023-03-17 17:27:44 +0000

    add pa11y testing step

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,6 @@ module.exports = {
     ci: {
         collect: {
             url: ['http://127.0.0.1:8000/'],
-            startServerCommand: 'pipenv run make serve',
             puppeteerScript: 'puppeteer-script.js'
         },
         upload: {
