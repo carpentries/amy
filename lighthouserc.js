3dc458f6c46a85b1561bcc6e9a29392d6dd6c64b
Eli Chadwick <elichadwick@carpentries.org>
2023-04-06 15:41:23 +0100

    reinstate startServerCommand

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,8 @@ module.exports = {
     ci: {
         collect: {
             url: ['http://127.0.0.1:8000/'],
-            puppeteerScript: 'puppeteer-script.js'
+            startServerCommand: 'pipenv run make serve',
+            puppeteerScript: 'puppeteer-script.js',
         },
         upload: {
             target: 'temporary-public-storage',
