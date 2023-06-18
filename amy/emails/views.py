e8cff676c268865f18502565c7f7116035fe0f15
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2023-06-18 20:14:51 +0200

    [#2433] Add explicit permission to the views

--- amy/emails/views.py
+++ amy/emails/views.py
@@ -4,6 +4,7 @@ from workshops.utils.access import OnlyForAdminsMixin
 
 
 class EmailTemplateListView(OnlyForAdminsMixin, AMYListView):
+    permission_required = ["emails.view_emailtemplate"]
     context_object_name = "email_templates"
     template_name = "emails/email_template_list.html"
     queryset = EmailTemplate.objects.order_by("name")
@@ -11,6 +12,7 @@ class EmailTemplateListView(OnlyForAdminsMixin, AMYListView):
 
 
 class ScheduledEmailListView(OnlyForAdminsMixin, AMYListView):
+    permission_required = ["emails.view_scheduledemail"]
     context_object_name = "scheduled_emails"
     template_name = "emails/scheduled_email_list.html"
     queryset = ScheduledEmail.objects.select_related("template").order_by("-created_at")
