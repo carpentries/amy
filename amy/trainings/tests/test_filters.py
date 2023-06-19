be575d423812b248166aba66652e9a9d330ac6ba
Eli Chadwick <elichadwick@carpentries.org>
2023-06-19 15:04:02 +0100

    get filter tests working with all_trainees_queryset

--- amy/trainings/tests/test_filters.py
+++ amy/trainings/tests/test_filters.py
@@ -1,11 +1,15 @@
 from datetime import datetime, timedelta
 
+from django.utils import timezone
+
 from trainings.filters import TraineeFilter
 from trainings.models import Involvement
 from trainings.views import all_trainees_queryset
 from workshops.models import (
     Event,
     Person,
+    Role,
+    Task,
     TrainingProgress,
     TrainingRequest,
     TrainingRequirement,
@@ -21,6 +25,7 @@ class TestTraineeFilter(TestBase):
     def setUp(self) -> None:
         super().setUp()  # create some persons
         self._setUpEvents()  # create some events
+        self._setUpRoles()  # create learner role
 
         self.model = Person
 
@@ -53,6 +58,16 @@ class TestTraineeFilter(TestBase):
             },
         )
 
+        # add training progress for Spiderman
+        # training request, event task, and welcome session
+        self.training_request = TrainingRequest.objects.create(
+            person=self.spiderman,
+            review_process="open",
+            personal="Peter",
+            family="Parker",
+            email="peter@webslinger.net",
+            state="a",
+        )
         self.event = Event.objects.create(
             start=datetime.today() + timedelta(days=-31),
             end=datetime.today() + timedelta(days=-30),
@@ -65,12 +80,19 @@ class TestTraineeFilter(TestBase):
             latitude=1,
             longitude=2,
         )
-
+        Task.objects.create(
+            event=self.event,
+            person=self.spiderman,
+            role=Role.objects.get(name="learner"),
+        )
         self.progress1 = TrainingProgress.objects.create(
             requirement=self.welcome,
             trainee=self.spiderman,
             state="p",
         )
+
+        # add some Get Involved progress to another trainee
+        # (pretending that it was submitted by them)
         self.progress2 = TrainingProgress.objects.create(
             requirement=self.get_involved,
             involvement_type=self.url_and_date_required,
@@ -79,25 +101,19 @@ class TestTraineeFilter(TestBase):
             date=datetime(2023, 5, 31),
             url="https://example.com",
         )
+
+        # add progress for a trainee who has their instructor badge
         self.progress3 = TrainingProgress.objects.create(
             requirement=self.welcome,
             trainee=self.hermione,
             state="p",
         )
 
-        self.training_request = TrainingRequest.objects.create(
-            person=self.spiderman,
-            review_process="open",
-            personal="Peter",
-            family="Parker",
-            email="peter@webslinger.net",
-            state="a",
-        )
-
         # get filterset
         self.filterset = TraineeFilter({})
 
-        # run most tests only on actual trainees we've set up
+        # use a queryset where trainee annotations are available (e.g. is_instructor)
+        # this still includes all Persons
         self.qs = all_trainees_queryset()
 
     def tearDown(self) -> None:
@@ -204,7 +220,8 @@ class TestTraineeFilter(TestBase):
         result = self.filterset.filters[filter_name].filter(self.qs, value)
 
         # Assert
-        self.assertQuerysetEqual(result, [self.spiderman, self.ironman])
+        self.assertIn(self.spiderman, result)
+        self.assertIn(self.ironman, result)
 
     def test_filter_training(self):
         # Arrange
@@ -221,18 +238,23 @@ class TestTraineeFilter(TestBase):
         # Arrange
         filter_name = "order_by"
         fields = self.filterset.filters[filter_name].param_map
+        qs = self.qs.filter(pk__in=[self.spiderman.pk, self.ironman.pk])
         results = {}
+        # none of these users have logged in, so manually set
+        self.spiderman.last_login = datetime(2023, 4, 1, 0, 0, 0, tzinfo=timezone.utc)
+        self.spiderman.save()
+        self.ironman.last_login = datetime(2023, 5, 1, 0, 0, 0, tzinfo=timezone.utc)
+        self.ironman.save()
+
         # default ordering is ascending
         expected_results = {
-            "last_login": [],
+            "last_login": [self.spiderman, self.ironman],
             "email": [self.ironman, self.spiderman],
         }
 
         # Act
         for field in fields.keys():
-            results[field] = self.filterset.filters[filter_name].filter(
-                self.qs, [field]
-            )
+            results[field] = self.filterset.filters[filter_name].filter(qs, [field])
 
         # Assert
         # we don't have any unexpected fields
