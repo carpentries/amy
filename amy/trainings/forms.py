614031a5f8dc5a4f0d61562e281f36b2f33bfe13
Eli Chadwick <elichadwick@carpentries.org>
2023-06-23 14:47:56 +0100

    include add_error trick on bulk progress form

--- amy/trainings/forms.py
+++ amy/trainings/forms.py
@@ -151,3 +151,14 @@ class BulkAddTrainingProgressForm(forms.ModelForm):
 
     class Media:
         js = ("trainingprogress_form.js",)
+
+    def add_error(self, field: str, error: ValidationError | str):
+        """Overrides add_error to ignore any errors that are intended to appear
+        on the trainee-only "trainee_notes" field."""
+        if field == "trainee_notes":
+            return
+        else:
+            if hasattr(error, "error_dict") and "trainee_notes" in error.error_dict:
+                error.error_dict.pop("trainee_notes")
+
+        return super().add_error(field, error)
