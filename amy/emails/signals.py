d72059d316de0e5eb3eb1df4db2465c47c46acb0
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2023-07-18 23:27:16 +0200

    [#2439] Base signal class with support for signal name

--- amy/emails/signals.py
+++ amy/emails/signals.py
@@ -1,8 +1,23 @@
-from django.dispatch import Signal
+from django.dispatch import Signal as DjangoSignal
+
+
+class Signal(DjangoSignal):
+    signal_name: str  # TODO: consider using enum
+
+    def __init__(self, *args, **kwargs):
+        self.signal_name = kwargs.pop("signal_name")
+        super().__init__(*args, **kwargs)
+
 
 # Scheduled to run immediately after action (so roughly up to 1 hour later).
-instructor_signs_up_for_workshop_signal = Signal()
-instructor_confirmed_for_workshop_signal = Signal()
-instructor_declined_from_workshop_signal = Signal()
-instructor_badge_awarded_signal = Signal()
-persons_merged_signal = Signal()
+instructor_badge_awarded_signal = Signal(signal_name="instructor_badge_awarded")
+instructor_confirmed_for_workshop_signal = Signal(
+    signal_name="instructor_confirmed_for_workshop"
+)
+instructor_declined_from_workshop_signal = Signal(
+    signal_name="instructor_declined_from_workshop"
+)
+instructor_signs_up_for_workshop_signal = Signal(
+    signal_name="instructor_signs_up_for_workshop"
+)
+persons_merged_signal = Signal(signal_name="persons_merged")
