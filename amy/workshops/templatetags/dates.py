8c1b5b07d845a996b1f117e447c8161f6f10ee45
Eli Chadwick <elichadwick@carpentries.org>
2023-07-18 12:59:53 +0100

    figured out the proper templating logic

--- amy/workshops/templatetags/dates.py
+++ amy/workshops/templatetags/dates.py
@@ -1,6 +1,5 @@
 from django import template
 from django.utils.safestring import mark_safe
-from django.utils.timezone import now
 
 from workshops.utils.dates import human_daterange as human_daterange_util
 
@@ -11,10 +10,3 @@ register = template.Library()
 def human_daterange(date_left, date_right):
     result = human_daterange_util(date_left, date_right)
     return mark_safe(result)
-
-
-@register.simple_tag
-def is_late_in_year():
-    """return True if current month is October, November, or December"""
-
-    return now().month >= 10
