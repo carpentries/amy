56b10d3270b0c723294e7ea9e947eda2f7291013
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2023-07-27 00:16:20 +0200

    [#2482] Display documentation links for variables used in template context

--- amy/emails/templatetags/emails.py
+++ amy/emails/templatetags/emails.py
@@ -1,5 +1,6 @@
 from django import template
 from django.conf import settings
+from django.db.models import Model
 
 register = template.Library()
 
@@ -10,3 +11,18 @@ def is_email_module_enabled() -> bool:
         return bool(settings.EMAIL_MODULE_ENABLED)
     except AttributeError:
         return False
+
+
+@register.filter
+def model_documentation_link(model: Model) -> str:
+    mapping = {
+        "InstructorRecruitmentSignup": (
+            "https://carpentries.github.io/amy/design/database_models/"
+            "#instructorrecruitmentsignup"
+        ),
+        "Event": "https://carpentries.github.io/amy/amy_database_structure/#events",
+        "Award": "https://carpentries.github.io/amy/design/database_models/#award",
+        "Person": "https://carpentries.github.io/amy/amy_database_structure/#persons",
+    }
+    model_name = model.__class__.__name__
+    return mapping.get(model_name, "")
