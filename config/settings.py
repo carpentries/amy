1881fd463a5e702d2c9f7052e8cf6784a231e6ac
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2023-08-13 17:04:51 +0200

    [#2485] Display feature flags in django debug toolbar

--- config/settings.py
+++ config/settings.py
@@ -551,6 +551,23 @@ LOGGING = {
 # -----------------------------------------------------------------------------
 INTERNAL_IPS = ["127.0.0.1", "::1"]
 DEBUG_TOOLBAR_CONFIG = {"SHOW_COLLAPSED": True}
+DEBUG_TOOLBAR_PANELS = [
+    "debug_toolbar.panels.history.HistoryPanel",
+    "debug_toolbar.panels.versions.VersionsPanel",
+    "debug_toolbar.panels.timer.TimerPanel",
+    "debug_toolbar.panels.settings.SettingsPanel",
+    "debug_toolbar.panels.headers.HeadersPanel",
+    "debug_toolbar.panels.request.RequestPanel",
+    "debug_toolbar.panels.sql.SQLPanel",
+    "debug_toolbar.panels.staticfiles.StaticFilesPanel",
+    "debug_toolbar.panels.templates.TemplatesPanel",
+    "debug_toolbar.panels.cache.CachePanel",
+    "debug_toolbar.panels.signals.SignalsPanel",
+    "debug_toolbar.panels.redirects.RedirectsPanel",
+    "debug_toolbar.panels.profiling.ProfilingPanel",
+    "flags.panels.FlagsPanel",
+    "flags.panels.FlagChecksPanel",
+]
 
 # Django-contrib-comments
 # -----------------------------------------------------------------------------
