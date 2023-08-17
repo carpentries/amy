890eae948f134cc5158589a1346ce5edcb6939b4
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2023-08-17 22:32:04 +0200

    [#2498] Add tests for the new template tag "message_allowed"

---
+++ amy/dashboard/tests/test_template_tags.py
@@ -0,0 +1,70 @@
+from django.contrib.messages import constants
+from django.contrib.messages.storage.base import Message
+from django.test import RequestFactory, TestCase
+
+from dashboard.templatetags.notifications import message_allowed
+from workshops.models import Person
+
+
+class TestMessageAllowed(TestCase):
+    def test_notification_not_tagged_for_admins_displays_for_admins(self) -> None:
+        # Arrange
+        person = Person.objects.create_superuser(
+            "admin", "admin", "admin", "admin@example.org", "admin"
+        )
+        request = RequestFactory().get("/")
+        request.user = person
+        message = Message(constants.INFO, "Test message", extra_tags="")
+
+        # Act
+        result = message_allowed(message, request)
+
+        # Assert
+        self.assertTrue(result)
+
+    def test_notification_not_tagged_for_admins_displays_for_instructors(self) -> None:
+        # Arrange
+        person = Person.objects.create_user(
+            "user", "user", "user", "user@example.org", "user"
+        )
+        request = RequestFactory().get("/")
+        request.user = person
+        message = Message(constants.INFO, "Test message", extra_tags="")
+
+        # Act
+        result = message_allowed(message, request)
+
+        # Assert
+        self.assertTrue(result)
+
+    def test_notification_tagged_for_admins_displays_for_admins(self) -> None:
+        # Arrange
+        person = Person.objects.create_superuser(
+            "admin", "admin", "admin", "admin@example.org", "admin"
+        )
+        request = RequestFactory().get("/")
+        request.user = person
+        message = Message(constants.INFO, "Test message", extra_tags="only-for-admins")
+
+        # Act
+        result = message_allowed(message, request)
+
+        # Assert
+        self.assertTrue(result)
+
+    def test_notification_tagged_for_admins_doesnt_display_for_instructors(
+        self,
+    ) -> None:
+        # Arrange
+        person = Person.objects.create_user(
+            "user", "user", "user", "user@example.org", "user"
+        )
+        request = RequestFactory().get("/")
+        request.user = person
+        message = Message(constants.INFO, "Test message", extra_tags="only-for-admins")
+
+        # Act
+        result = message_allowed(message, request)
+
+        # Assert
+        self.assertFalse(result)
