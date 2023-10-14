83f3bc2b2a433820a8288c28f6305482cfbb2c8d
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2023-10-14 23:36:46 +0200

    [#2530] Move another 3 actions to BaseAction

--- amy/emails/actions/persons_merged.py
+++ amy/emails/actions/persons_merged.py
@@ -1,47 +1,37 @@
-from typing import Any
+from datetime import datetime
 
-from django.dispatch import receiver
 from typing_extensions import Unpack
 
-from emails.controller import EmailController, EmailControllerMissingRecipientsException
-from emails.models import EmailTemplate
+from emails.actions.base_action import BaseAction
 from emails.signals import persons_merged_signal
 from emails.types import PersonsMergedContext, PersonsMergedKwargs
-from emails.utils import (
-    immediate_action,
-    messages_action_scheduled,
-    messages_missing_recipients,
-    messages_missing_template,
-    person_from_request,
-)
+from emails.utils import immediate_action
 from workshops.models import Person
-from workshops.utils.feature_flags import feature_flag_enabled
 
 
-@receiver(persons_merged_signal)
-@feature_flag_enabled("EMAIL_MODULE")
-def persons_merged_receiver(sender: Any, **kwargs: Unpack[PersonsMergedKwargs]) -> None:
-    request = kwargs["request"]
-    selected_person_id = kwargs["selected_person_id"]
-
-    scheduled_at = immediate_action()
-    person = Person.objects.get(pk=selected_person_id)
-    context: PersonsMergedContext = {
-        "person": person,
-    }
+class PersonsMergedReceiver(BaseAction):
     signal = persons_merged_signal.signal_name
-    try:
-        scheduled_email = EmailController.schedule_email(
-            signal=signal,
-            context=context,
-            scheduled_at=scheduled_at,
-            to_header=[person.email] if person.email else [],
-            generic_relation_obj=person,
-            author=person_from_request(request),
-        )
-    except EmailControllerMissingRecipientsException:
-        messages_missing_recipients(request, signal)
-    except EmailTemplate.DoesNotExist:
-        messages_missing_template(request, signal)
-    else:
-        messages_action_scheduled(request, signal, scheduled_email)
+
+    def get_scheduled_at(self, **kwargs) -> datetime:
+        return immediate_action()
+
+    def get_context(
+        self, **kwargs: Unpack[PersonsMergedKwargs]
+    ) -> PersonsMergedContext:
+        person = Person.objects.get(pk=kwargs["selected_person_id"])
+        return {
+            "person": person,
+        }
+
+    def get_generic_relation_object(
+        self, context: PersonsMergedContext, **kwargs
+    ) -> Person:
+        return context["person"]
+
+    def get_recipients(self, context: PersonsMergedContext, **kwargs) -> list[str]:
+        person = context["person"]
+        return [person.email] if person.email else []
+
+
+persons_merged_receiver = PersonsMergedReceiver()
+persons_merged_signal.connect(persons_merged_receiver)
