fe098a5a332c3e57b3cd6f1d7dee06c780b909b5
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2023-10-28 21:35:16 +0200

    [#2540] Run strategy in some views

--- amy/trainings/views.py
+++ amy/trainings/views.py
@@ -4,6 +4,10 @@ from django.db.models import Case, Count, F, IntegerField, Prefetch, Sum, When
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 
+from emails.actions.instructor_training_completed_not_badged import (
+    instructor_training_completed_not_badged_strategy,
+    run_instructor_training_completed_not_badged_strategy,
+)
 from trainings.filters import TraineeFilter
 from trainings.forms import BulkAddTrainingProgressForm, TrainingProgressForm
 from trainings.utils import raise_validation_error_if_no_learner_task
@@ -74,16 +78,55 @@ class TrainingProgressCreate(
         context["form"].helper = context["form"].create_helper
         return context
 
+    def form_valid(self, form):
+        person = form.cleaned_data["trainee"]
+        event = form.cleaned_data["event"]
+        result = super().form_valid(form)
+        run_instructor_training_completed_not_badged_strategy(
+            instructor_training_completed_not_badged_strategy(person),
+            request=self.request,
+            person=person,
+            training_completed_date=event.end if event else None,
+        )
+        return result
+
 
 class TrainingProgressUpdate(RedirectSupportMixin, OnlyForAdminsMixin, AMYUpdateView):
     model = TrainingProgress
     form_class = TrainingProgressForm
     template_name = "trainings/trainingprogress_form.html"
 
+    def form_valid(self, form):
+        person = form.cleaned_data["trainee"]
+        event = form.cleaned_data["event"]
+        run_instructor_training_completed_not_badged_strategy(
+            instructor_training_completed_not_badged_strategy(person),
+            request=self.request,
+            person=person,
+            training_completed_date=event.end if event else None,
+        )
+        return super().form_valid(form)
+
 
 class TrainingProgressDelete(RedirectSupportMixin, OnlyForAdminsMixin, AMYDeleteView):
     model = TrainingProgress
     success_url = reverse_lazy("all_trainees")
+    object: TrainingProgress
+
+    def before_delete(self, *args, **kwargs):
+        """Save for use in `after_delete` method."""
+        self._person = self.object.trainee
+        self._event = self.object.event
+
+    def after_delete(self, *args, **kwargs):
+        person = self._person
+        event = self._event
+        run_instructor_training_completed_not_badged_strategy(
+            instructor_training_completed_not_badged_strategy(person),
+            request=self.request,
+            person=person,
+            training_completed_date=event.end if event else None,
+        )
 
 
 def all_trainees_queryset():
@@ -144,6 +187,14 @@ def all_trainees(request):
                     )
                     progress.full_clean()
                     progress.save()
+
+                    run_instructor_training_completed_not_badged_strategy(
+                        instructor_training_completed_not_badged_strategy(trainee),
+                        request=request,
+                        person=trainee,
+                        training_completed_date=event.end if event else None,
+                    )
+
                 except ValidationError as e:
                     unique_constraint_message = (
                         "Training progress with this Trainee "
