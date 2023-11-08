5b423d8c3be782dbe5b72d00a735a47fae562c88
Eli Chadwick <elichadwick@carpentries.org>
2023-11-08 10:36:22 +0000

    move membership info to template tag

---
+++ amy/extrequests/templatetags/membership_active.py
@@ -0,0 +1,30 @@
+from datetime import date
+
+from django import template
+from django.utils.safestring import mark_safe
+
+from workshops.models import Membership
+
+register = template.Library()
+
+
+@register.simple_tag
+def build_membership_info(membership: Membership):
+    workshops_remaining = membership.workshops_without_admin_fee_remaining
+    active = membership.active_on_date(date.today())
+    if workshops_remaining <= 0 or not active:
+        alert_type = "warning"
+    else:
+        alert_type = "info"
+    info = (
+        f'<div class="alert alert-{alert_type}">'
+        f'Related membership: <a href="{membership.get_absolute_url}">{membership}</a>.'
+        "<br>"
+        f"This membership has <strong>{workshops_remaining}</strong> "
+        f'workshop{"s" if workshops_remaining !=1 else ""} remaining.'
+    )
+    if not active:
+        info += "<br>This membership is <strong>not currently active</strong>."
+    info += "</div>"
+
+    return mark_safe(info)
