97989164a5cac0d837cfb6e3826c4e5ae391242d
Eli Chadwick <elichadwick@carpentries.org>
2023-11-14 17:57:13 +0000

    update tests

--- amy/extrequests/tests/test_template_tags.py
+++ amy/extrequests/tests/test_template_tags.py
@@ -2,11 +2,14 @@ from datetime import date, timedelta
 
 from django.test import TestCase
 
-from amy.extrequests.templatetags.request_membership import membership_description
+from amy.extrequests.templatetags.request_membership import (
+    membership_active,
+    membership_alert_type,
+)
 from workshops.models import Membership
 
 
-class TestMembershipDescription(TestCase):
+class TestMembershipAlertType(TestCase):
     def setUp(self):
         self.membership = Membership.objects.create(
             name="Alpha Organization",
@@ -18,76 +21,80 @@ class TestMembershipDescription(TestCase):
             workshops_without_admin_fee_per_agreement=2,
         )
 
-    def test_not_a_membership(self):
+    def test_active_and_has_workshops(self):
         # Arrange
-        expected = ""
+        expected = "info"
 
         # Act
-        result = membership_description("some-string")
+        result = membership_alert_type(self.membership)
 
         # Assert
         self.assertEqual(expected, result)
 
-    def test_active_and_has_workshops(self):
-        # Arrange
-        expected = (
-            '<div class="alert alert-info">Related membership:'
-            f'<a href="{self.membership.get_absolute_url()}">{self.membership}</a>.<br>'
-            "This membership has <strong>2</strong> workshops remaining.</div>"
-        )
-
-        # Act
-        result = membership_description(self.membership)
-
-        # Assert
-        self.assertHTMLEqual(expected, result)
-
     def test_active_and_no_workshops(self):
         # Arrange
         self.membership.workshops_without_admin_fee_per_agreement = 0
-        expected = (
-            '<div class="alert alert-warning">Related membership:'
-            f'<a href="{self.membership.get_absolute_url()}">{self.membership}</a>.<br>'
-            "This membership has <strong>0</strong> workshops remaining.</div>"
-        )
+        expected = "warning"
 
         # Act
-        result = membership_description(self.membership)
+        result = membership_alert_type(self.membership)
 
         # Assert
-        self.assertHTMLEqual(expected, result)
+        self.assertEqual(expected, result)
 
     def test_inactive_and_no_workshops(self):
         # Arrange
         self.membership.workshops_without_admin_fee_per_agreement = 0
         self.membership.agreement_end = date.today() - timedelta(days=1)
-        expected = (
-            '<div class="alert alert-warning">Related membership:'
-            f'<a href="{self.membership.get_absolute_url()}">{self.membership}</a>.<br>'
-            "This membership has <strong>0</strong> workshops remaining."
-            "<br>This membership is <strong>not currently active</strong>."
-            "</div>"
-        )
+        expected = "warning"
 
         # Act
-        result = membership_description(self.membership)
+        result = membership_alert_type(self.membership)
 
         # Assert
-        self.assertHTMLEqual(expected, result)
+        self.assertEqual(expected, result)
 
     def test_inactive_and_has_workshops(self):
         # Arrange
         self.membership.agreement_end = date.today() - timedelta(days=1)
-        expected = (
-            '<div class="alert alert-warning">Related membership:'
-            f'<a href="{self.membership.get_absolute_url()}">{self.membership}</a>.<br>'
-            "This membership has <strong>2</strong> workshops remaining."
-            "<br>This membership is <strong>not currently active</strong>."
-            "</div>"
-        )
+        expected = "warning"
 
         # Act
-        result = membership_description(self.membership)
+        result = membership_alert_type(self.membership)
 
         # Assert
-        self.assertHTMLEqual(expected, result)
+        self.assertEqual(expected, result)
+
+
+class TestMembershipActive(TestCase):
+    def setUp(self):
+        self.membership = Membership.objects.create(
+            name="Alpha Organization",
+            variant="bronze",
+            agreement_start=date.today() - timedelta(weeks=26),
+            agreement_end=date.today() + timedelta(weeks=26),
+            contribution_type="financial",
+            registration_code="valid123",
+            workshops_without_admin_fee_per_agreement=2,
+        )
+
+    def test_active(self):
+        # Arrange
+        expected = True
+
+        # Act
+        result = membership_active(self.membership)
+
+        # Assert
+        self.assertEqual(expected, result)
+
+    def test_inactive(self):
+        # Arrange
+        self.membership.agreement_end = date.today() - timedelta(days=1)
+        expected = False
+
+        # Act
+        result = membership_active(self.membership)
+
+        # Assert
+        self.assertEqual(expected, result)
