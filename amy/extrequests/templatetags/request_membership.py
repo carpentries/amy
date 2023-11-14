39ae00cc89b380437d01c5876a41703cdeb2fe36
Eli Chadwick <elichadwick@carpentries.org>
2023-11-14 17:51:18 +0000

    move description from template tag to template

--- amy/extrequests/templatetags/request_membership.py
+++ amy/extrequests/templatetags/request_membership.py
@@ -1,7 +1,6 @@
 from datetime import date
 
 from django import template
-from django.utils.safestring import mark_safe
 
 from workshops.models import Membership
 
@@ -9,26 +8,15 @@ register = template.Library()
 
 
 @register.simple_tag
-def membership_description(membership: Membership):
-    if not isinstance(membership, Membership):
-        return ""
-
+def membership_alert_type(membership: Membership) -> str:
     workshops_remaining = membership.workshops_without_admin_fee_remaining
     active = membership.active_on_date(date.today())
     if workshops_remaining <= 0 or not active:
-        alert_type = "warning"
+        return "warning"
     else:
-        alert_type = "info"
-    info = (
-        f'<div class="alert alert-{alert_type}">'
-        "Related membership: "
-        f'<a href="{membership.get_absolute_url()}">{membership}</a>.'
-        "<br>"
-        f"This membership has <strong>{workshops_remaining}</strong> "
-        f'workshop{"s" if workshops_remaining !=1 else ""} remaining.'
-    )
-    if not active:
-        info += "<br>This membership is <strong>not currently active</strong>."
-    info += "</div>"
+        return "info"
 
-    return mark_safe(info)
+
+@register.simple_tag
+def membership_active(membership: Membership) -> bool:
+    return membership.active_on_date(date.today())
