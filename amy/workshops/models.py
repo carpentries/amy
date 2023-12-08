803453004bf88f9b15eab06c8921136566d8f794
Maneesha Sane <ahseenam@gmail.com>
2023-12-08 11:13:24 -0500

    rm prompt re:online/inperson from other info ques on wkshp req form

--- amy/workshops/models.py
+++ amy/workshops/models.py
@@ -3151,10 +3151,8 @@ class WorkshopRequest(
     )
     user_notes = models.TextField(
         blank=True,
-        verbose_name="Will this workshop be conducted in-person or online? "
-        "Is there any other information you would like to share "
-        "with us?",
-        help_text="Knowing if this workshop is on-line or in-person will "
+        verbose_name="Is there any other information you would like to share with us?",
+        help_text="Please share any other information to "
         "help ensure we can best support you in coordinating the event.",
     )
 
