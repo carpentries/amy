fbc11d05283930f5f04c233bd06dde13fbde976c
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2023-12-17 18:12:05 +0100

    [#2527] Add Pydantic models with some basic validation for the JSON fields in ScheduledEmail

---
+++ amy/emails/schemas.py
@@ -0,0 +1,32 @@
+from functools import partial
+from typing import Annotated
+from urllib.parse import urlparse
+
+from pydantic import AfterValidator, BaseModel, RootModel
+
+
+def uri_validator(uri: str, expected_scheme: str = "https") -> str:
+    try:
+        result = urlparse(uri)
+        if result.scheme == expected_scheme:
+            return uri
+        raise ValueError("Invalid URI1")
+    except AttributeError:
+        raise ValueError("Invalid URI2")
+
+
+ApiUri = Annotated[str, AfterValidator(partial(uri_validator, expected_scheme="api"))]
+
+
+class SinglePropertyLinkModel(BaseModel):
+    # custom URI for links to individual models in API, e.g. "api:person/1234"
+    api_uri: ApiUri
+    property: str
+
+
+class ToHeaderModel(RootModel[list[SinglePropertyLinkModel]]):
+    ...
+
+
+class ContextModel(RootModel[dict[str, ApiUri]]):
+    ...
