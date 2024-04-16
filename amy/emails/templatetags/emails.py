6c7defbbbb7aaceaf72818fbfdbd12d259180787
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2024-04-16 23:15:34 +0200

    [#2622] Add includable template to list related scheduled emails

--- amy/emails/templatetags/emails.py
+++ amy/emails/templatetags/emails.py
@@ -1,6 +1,12 @@
 from django import template
+from django.contrib.contenttypes.models import ContentType
+from django.db.models import Model, QuerySet
 
-from emails.models import ScheduledEmailStatus, ScheduledEmailStatusActions
+from emails.models import (
+    ScheduledEmail,
+    ScheduledEmailStatus,
+    ScheduledEmailStatusActions,
+)
 
 register = template.Library()
 
@@ -12,3 +18,12 @@ def allowed_actions_for_status(status: ScheduledEmailStatus) -> list[str]:
         for key, statuses in ScheduledEmailStatusActions.items()
         if status in statuses
     ]
+
+
+@register.simple_tag
+def get_related_scheduled_emails(obj: Model) -> QuerySet[ScheduledEmail]:
+    content_type = ContentType.objects.get_for_model(obj.__class__)
+    return ScheduledEmail.objects.filter(
+        generic_relation_content_type=content_type,
+        generic_relation_pk=obj.pk,
+    ).order_by("-created_at")
