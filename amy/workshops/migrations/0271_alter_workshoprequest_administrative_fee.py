cabec3dbe87fcdb36f8a09afc349fced598089ea
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2024-10-07 23:04:27 +0200

    [#2582] Reorder migrations

--- amy/workshops/migrations/0271_alter_workshoprequest_administrative_fee.py
+++ amy/workshops/migrations/0271_alter_workshoprequest_administrative_fee.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('workshops', '0269_alter_membership_agreement_link'),
+        ('workshops', '0270_alter_organization_affiliated_organizations'),
     ]
 
     operations = [
