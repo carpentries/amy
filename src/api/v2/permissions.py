d6ee9160251c8d2d28ad83f417792c0dcb1a88c4
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2025-12-26 18:46:55 +0100

    feat(#2757): Fix mypy issues in `api` app

--- src/api/v2/permissions.py
+++ src/api/v2/permissions.py
@@ -1,8 +1,10 @@
 from rest_framework.permissions import BasePermission
+from rest_framework.request import Request
+from rest_framework.views import APIView
 
 
 class ApiAccessPermission(BasePermission):
     _permission_name = "knox.add_authtoken"
 
-    def has_permission(self, request, view):
+    def has_permission(self, request: Request, view: APIView) -> bool:
         return request.user.has_perm(self._permission_name)
